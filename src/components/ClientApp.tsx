d059078e2fb8a969ca31102ba06b3e31504603f0
roms <rusirusamaraweera749@gmail.com>
2024-02-27 11:34:41 +0530

    add upperNav

---
+++ src/components/ClientApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UpperNav from './Navbars/UpperNav.tsx'
+
+const ClientApp = () => {
+
+  return (
+    <>
+        <UpperNav /> 
+        
+    </>
+  )
+}
+
+export default ClientApp;
