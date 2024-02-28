2122f54bba1d18686bcb9224ae37a4883d30fbff
roms <rusirusamaraweera749@gmail.com>
2024-02-28 11:33:46 +0530

    create cart badge

---
+++ src/components/Navbars/CartBadge.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { MinusOutlined, PlusOutlined, QuestionOutlined } from '@ant-design/icons';
+import { Avatar, Badge, Button, Switch, Space } from 'antd';
+
+const ButtonGroup = Button.Group;
+
+const App: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+
+  const decline = () => {
+    let newCount = count - 1;
+    if (newCount < 0) {
+      newCount = 0;
+    }
+    setCount(newCount);
+  };
+
+
+  return (
+    <Space direction="vertical">
+      <Space size="large">
+        <Badge count={count}>
+          <Avatar shape="square" size="large" />
+        </Badge>
+        <ButtonGroup>
+          <Button onClick={decline} icon={<MinusOutlined />} />
+          <Button onClick={increase} icon={<PlusOutlined />} />
+        </ButtonGroup>
+      </Space>
+      <Space size="large">
+        <Badge dot={show}>
+          <Avatar shape="square" size="large" />
+        </Badge>
+        <Switch onChange={onChange} checked={show} />
+      </Space>
+    </Space>
+  );
+};
+
+export default App;
