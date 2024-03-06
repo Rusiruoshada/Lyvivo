37f937b90620f42cf38ef420e68a4a574a027c30
roms <rusirusamaraweera749@gmail.com>
2024-03-06 11:57:27 +0530

    add categories to home page

--- src/components/ClientApp.tsx
+++ src/components/ClientApp.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import UpperNav from './Navbars/UpperNav.tsx'
 import BottomNav from './Navbars/BottomNav.tsx';
+import ImageSlider from './Carousel/Carousel.tsx';
+import CategorySpace from './HomeShopByCategory/CategorySpace.tsx'
 
-const ClientApp = () => {
+const ClientApp: React.FC  = () => {
 
   return (
     <>
         <UpperNav /> 
         <BottomNav />
-        
+        <ImageSlider />
+        <CategorySpace />
     </>
   )
 }
