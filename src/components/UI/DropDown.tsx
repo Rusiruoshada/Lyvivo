33368125ef6a89792ba659385237b7c37f64422b
roms <rusirusamaraweera749@gmail.com>
2024-03-07 16:33:35 +0530

    modify dropDown

--- src/components/UI/DropDown.tsx
+++ src/components/UI/DropDown.tsx
@@ -1,24 +1,24 @@
-import { Cascader } from 'antd';
+import { Cascader, Dropdown } from 'antd';
 import React from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
-import {categories1} from '../HomeShopByCategory/exportCategoryObject.tsx'
+import { categories1 } from '../HomeShopByCategory/exportCategoryObject.tsx';
 
 interface DropDownProps {
   categoryName: string;
 }
 
-const onChange = (value: string[]) => {
+const onChange: any = (value: string[]) => {
   console.log(value);
 };
 
 const DropDown: React.FC<DropDownProps> = ({ categoryName }) => {
   return (
     <Cascader
-    options={categories1}
-    expandTrigger="hover"
-    style={{border:'none',width:'100%'}}
-  >
-    <p>{categoryName}</p>
+      options={categories1}
+      expandTrigger='hover'
+      style={{ border: 'none', width: '100%', cursor: 'pointer' }}
+      onChange={onChange}
+    >
+      <div className='cursor-pointer'>{categoryName}</div>
     </Cascader>
   );
 };
