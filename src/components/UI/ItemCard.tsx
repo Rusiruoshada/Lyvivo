4482ba2e739da0811ee04be2f636baf1f8720566
roms <rusirusamaraweera749@gmail.com>
2024-03-10 22:40:20 +0530

    modify card design

--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -1,30 +1,29 @@
 import React from 'react';
-import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
-
+import { Button, Card } from 'antd';
+import { BsCart2 } from 'react-icons/bs';
 const { Meta } = Card;
 
-const App: React.FC = () => (
+const ItemCard: React.FC = () => (
   <Card
     style={{ width: 300 }}
-    cover={
-      <img
-        alt="example"
-        src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
-      />
-    }
+    cover={<img alt='example' src='./images/Pic923056.jpg' />}
     actions={[
-      <SettingOutlined key="setting" />,
-      <EditOutlined key="edit" />,
-      <EllipsisOutlined key="ellipsis" />,
+      <Button
+        style={{
+          width: '90%',
+          color: 'var(--primaryColor)',
+          borderColor: 'var(--primaryColor)',
+          
+        }}
+        className='hover:bg-[var(--primaryColor)] hover:color-white '
+        icon=<BsCart2 />
+      >
+        Add to Cart
+      </Button>,
     ]}
   >
-    <Meta
-      avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />}
-      title="Card title"
-      description="This is the description"
-    />
+    <Meta title='Card title' description='This is the description' />
   </Card>
 );
 
-export default App;
+export default ItemCard;
