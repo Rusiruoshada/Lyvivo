1879f52dbf73f228ced408e8b1f8f3591fb7b777
roms <rusirusamaraweera749@gmail.com>
2024-03-10 22:40:40 +0530

    modify bottom navbar

--- src/components/Navbars/BottomNav.tsx
+++ src/components/Navbars/BottomNav.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartBadge from './CartBadge.tsx';
 import SearchBar from '../SearchBar/SearchBar.tsx';
 import DropDown from '../UI/DropDown.tsx';
@@ -19,57 +18,55 @@ const BottomNav: React.FC = () => {
   };
 
   return (
-      <Navbar
-        collapseOnSelect
-        expand='lg'
-        className='sticky top-0 bottom-auto left-0 right-0 z-10 bg-white shadow-md drop-shadow-sm'
-        sticky='top'
-      >
-        <Container>
-          <Navbar.Brand
-            href='/'
-            style={{
-              fontWeight: '700',
-              color: 'var(--primaryColor)',
-              fontSize: '2rem',
-            }}
+    <Navbar
+      collapseOnSelect
+      expand='lg'
+      className='sticky top-0 bottom-auto left-0 right-0 z-10 bg-white shadow-md drop-shadow-sm'
+      sticky='top'
+    >
+      <Container>
+        <Navbar.Brand
+          href='/'
+          style={{
+            fontWeight: '700',
+            color: 'var(--primaryColor)',
+            fontSize: '2rem',
+          }}
+        >
+          Lyvivo
+        </Navbar.Brand>
+        <div className='flex gap-3 relative'>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse
+            id='responsive-navbar-nav'
+            className=' flex justify-end absolute lg:relative top-[3rem] lg:top-0 right-3 lg:right-0 left-auto bg-white lg:bg-transparent shadow-md lg:shadow-none lg:p-0  '
+            style={{ padding: '0.5rem', zIndex: 10 }}
           >
-            Lyvivo
-          </Navbar.Brand>
-          <div className='flex gap-3 relative'>
-            <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-            <Navbar.Collapse
-              id='responsive-navbar-nav'
-              className=' flex justify-end absolute lg:relative top-[3rem] lg:top-0 right-3 lg:right-0 left-auto bg-white lg:bg-transparent shadow-md lg:shadow-none lg:p-0  '
-              style={{ padding: '1rem' }}
-            >
-              <Nav className='flex justify-end '>
-
-                  <DropDown />
-                {navLinks.navLinks.map((link) => (
-                  <Nav.Link
-                    className='hover:font-medium hover:text-[var(--primaryColor)] hover:border  text-gray-400  transition-all duration-500'
-                    href={'/' + link.toLowerCase()}
-                    key={link}
-                  >
-                    {link}
-                  </Nav.Link>
-                ))}
-              </Nav>
-            </Navbar.Collapse>
-            <Nav>
-              <Nav.Link
-                href='#deets'
-                style={{ display: 'flex', alignItems: 'center' }}
-              >
-                <CartBadge />
-              </Nav.Link>
+            <Nav className='flex justify-end '>
+              <DropDown />
+              {navLinks.navLinks.map((link) => (
+                <Nav.Link
+                  className='hover:font-medium hover:text-[var(--primaryColor)] hover:border  text-gray-400  transition-all duration-500'
+                  href={'/' + link.toLowerCase()}
+                  key={link}
+                >
+                  {link}
+                </Nav.Link>
+              ))}
             </Nav>
-          </div>
-        </Container>
-        <SearchBar />
-      </Navbar>
-
+          </Navbar.Collapse>
+          <Nav>
+            <Nav.Link
+              href='#cart'
+              style={{ display: 'flex', alignItems: 'center' }}
+            >
+              <CartBadge />
+            </Nav.Link>
+          </Nav>
+        </div>
+      </Container>
+      <SearchBar />
+    </Navbar>
   );
 };
 
