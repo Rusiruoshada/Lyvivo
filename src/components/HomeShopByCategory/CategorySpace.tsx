380204af2a0bafd64be34d11f6e83c952c2a31e1
roms <rusirusamaraweera749@gmail.com>
2024-03-12 16:50:46 +0530

    change responsive in category space

--- src/components/HomeShopByCategory/CategorySpace.tsx
+++ src/components/HomeShopByCategory/CategorySpace.tsx
@@ -1,29 +1,42 @@
 import React from 'react';
-import { Card } from 'antd';
-import categories from './exportCategoryObject.tsx';
+import { Card, Col, Row } from 'antd';
+import categories1 from './exportCategoryObject.tsx';
 import CategoryCard from './CategoryCards.tsx';
 
 const gridStyle: React.CSSProperties = {
-  width: '15%',
+  width: '100%',
   textAlign: 'center',
 };
 
 const CategorySpace: React.FC = () => (
   <Card
     title={
-      <h3 className='mb-10 font-bold text-[var(--primaryColor)]'>
+      <h3 className='mb-3 sm:mb-3 md:mb-10 font-bold text-[var(--primaryColor)]'>
         Shop by Category
       </h3>
     }
-    className='my-10 p-10 text-center border-none'
+    className='my-5 sm:my-5 md:my-20 lg:my-20 p-0 text-center border-none'
   >
-    {categories.map((categoryItem) => (
-      <Card.Grid style={gridStyle} className='p-0 m-auto'>
-        <CategoryCard
-          category={categories}
-        />
-      </Card.Grid >
-    ))}
+    <Row gutter={20} justify='center' className='p-0 gap-3'>
+      {categories1.map((categoryItem) => (
+        <Col
+          className='py-3 sm:py-2 md:p-5 lg:p-10 px-0 scale-100 sm:scale-100 md:scale-75 lg:scale-75'
+          xs={10}
+          sm={10}
+          md={7}
+          lg={4}
+          key={categoryItem.label}
+        >
+          <Card.Grid style={gridStyle} className='p-0'>
+            <CategoryCard
+              categoryName={categoryItem.value}
+              categoryImg={categoryItem.imgPath}
+              key={categoryItem.value}
+            />
+          </Card.Grid>
+        </Col>
+      ))}
+    </Row>
   </Card>
 );
 
