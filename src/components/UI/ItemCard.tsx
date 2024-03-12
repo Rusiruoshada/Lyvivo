4372d2da965159a48c53c4b54f3365968745dc2a
roms <rusirusamaraweera749@gmail.com>
2024-03-12 11:56:01 +0530

    modify item card style

--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -5,20 +5,23 @@ import { BsCart2 } from 'react-icons/bs';
 
 const { Meta } = Card;
 
-const ItemCard: React.FC = () => (
+const ItemCard: React.FC = () => {
+
+    const cardTitle = 'hello kitty'
+
+    return (  
   <Card
     style={{ width: 300 }}
     cover={<img className='aspect-square' alt='example' src='./images/Pic923056.jpg' />}
-    className='scale-75 hover:shadow-md tran'
+    className='scale-75 hover:shadow-lg hover:scale-[0.76] border'
     actions={[
       <Button
         style={{
           width: '90%',
           color: 'var(--primaryColor)',
           borderColor: 'var(--primaryColor)',
-          
         }}
-        className='bg-[var(--primaryColor)] text-white text-2xl flex items-center justify-center'
+        className='hover:!bg-[var(--primaryColor)] hover:!text-white text-2xl flex items-center justify-center'
         size='large'
         icon=<BsCart2 />
       >
@@ -28,8 +31,8 @@ const ItemCard: React.FC = () => (
   >
     <Badge status='success' className='border-2 px-2 rounded aspect-square mb-2' size='default' />
 
-    <Meta title='Card title' description='This is the description' />
+    <Meta title=<h4 className='font-medium  capitalize'>{cardTitle}</h4> description='This is the description' />
   </Card>
-);
+)};
 
 export default ItemCard;
