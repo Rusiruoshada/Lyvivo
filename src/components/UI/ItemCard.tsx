723c0739496698a9edff8f6d293863b1527d95ce
roms <rusirusamaraweera749@gmail.com>
2024-03-14 17:07:40 +0530

    modify item card

--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -1,38 +1,59 @@
 import React from 'react';
-import { Button, Card,Badge } from 'antd';
+import { Button, Card, Badge } from 'antd';
 import { BsCart2 } from 'react-icons/bs';
-
+import DropDown from './DropDown.tsx';
+import categories from '../HomeShopByCategory/exportCategoryObject.tsx';
 
 const { Meta } = Card;
 
 const ItemCard: React.FC = () => {
+  const cardTitle: string = 'hello kitty';
+  const cardDescription: string = '(Inclusive of all taxes)';
+  const cardPrice: string = parseFloat('32.00').toFixed(2);
+  const badgeRibbonText: string = 'In Stock';
 
-    const cardTitle = 'hello kitty'
-
-    return (  
-  <Card
-    style={{ width: 300 }}
-    cover={<img className='aspect-square' alt='example' src='./images/Pic923056.jpg' />}
-    className='scale-75 hover:shadow-lg hover:scale-[0.76] border'
-    actions={[
-      <Button
-        style={{
-          width: '90%',
-          color: 'var(--primaryColor)',
-          borderColor: 'var(--primaryColor)',
-        }}
-        className='hover:!bg-[var(--primaryColor)] hover:!text-white text-2xl flex items-center justify-center'
-        size='large'
-        icon=<BsCart2 />
-      >
-        Add to Cart
-      </Button>,
-    ]}
-  >
-    <Badge status='success' className='border-2 px-2 rounded aspect-square mb-2' size='default' />
-
-    <Meta title=<h4 className='font-medium  capitalize'>{cardTitle}</h4> description='This is the description' />
-  </Card>
-)};
+  return (
+    <Card
+      style={{ width: 300 }}
+      cover={
+        <Badge.Ribbon text={badgeRibbonText} color='green'>
+          <img
+            className='aspect-square '
+            alt='example'
+            src='./images/Pic923056.jpg'
+          />
+        </Badge.Ribbon>
+      }
+      className='scale-75 hover:shadow-lg hover:scale-[0.76] border-2'
+      actions={[
+        <Button
+          style={{
+            width: '90%',
+            color: 'var(--primaryColor)',
+            borderColor: 'var(--primaryColor)',
+          }}
+          className='hover:!bg-[var(--primaryColor)] hover:!text-white text-[18px]'
+          size='large'
+          icon=<BsCart2 />
+        >
+          Add to Cart
+        </Button>,
+      ]}
+    >
+      <Meta title=<h4 className='font-bold  capitalize'>{cardTitle}</h4> />
+      <DropDown
+        placeholderName='place'
+        categories={categories}
+        className='[& >div]:bg-gray-200 w-100'
+      />
+      <Meta
+        title=<h4 className='font-medium  m-0 capitalize'>Rs. {cardPrice}</h4>
+        description=<p className='text-[18px]  m-0 capitalize'>
+          {cardDescription}
+        </p>
+      />
+    </Card>
+  );
+};
 
 export default ItemCard;
