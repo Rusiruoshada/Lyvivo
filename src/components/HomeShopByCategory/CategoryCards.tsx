7b8b4b6af1a6119081cb452669c064b13febf264
roms <rusirusamaraweera749@gmail.com>
2024-03-17 16:07:40 +0530

    modify category cards

--- src/components/HomeShopByCategory/CategoryCards.tsx
+++ src/components/HomeShopByCategory/CategoryCards.tsx
@@ -1,33 +1,51 @@
 import React from 'react';
 import { Card } from 'antd';
-import DropDown from '../UI/DropDown.tsx';
 
 const { Meta } = Card;
 
 interface CategoryCardProps {
-  categoryName:string;
+  categoryName: string;
   categoryImg: string;
 }
 
 const CategoryCard: React.FC<CategoryCardProps> = ({
-    categoryName,categoryImg
-}) => (
-  <Card
-    style={{ width: '100%',objectFit:'cover',padding:'10px',border:0 }}
-    cover={<img alt='all' src={categoryImg}/>}
-  >
-    <Meta
-      title=<DropDown categoryName={categoryName} />
+  categoryName,
+  categoryImg,
+}) => {
+  const onCardClick = (event: any) => {
+    alert(event.target.ariaLabel);
+  };
+
+  return (
+    <Card
       style={{
-        display: 'flex',
-        justifyContent: 'center',
         width: '100%',
-        textAlign: 'center',
-        margin: '10px auto',
-        
+        objectFit: 'cover',
+        padding: '10px',
+        cursor: 'pointer',
       }}
-    />
-  </Card>
-);
+      cover={
+        <img aria-label={categoryName} alt={categoryName} src={categoryImg} />
+      }
+      aria-label={categoryName}
+      onClick={onCardClick}
+      className='last:[&_div]:p-0 '
+    >
+      <Meta
+        title=<p className='m-0 py-3 text-[12px] sm:text-[12px] md:text-[18px] lg:text-[18px] ' aria-label={categoryName}>
+          {categoryName}
+        </p>
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          textAlign: 'center',
+          margin: '0px auto',
+          padding: 0,
+        }}
+      />
+    </Card>
+  );
+};
 
 export default CategoryCard;
