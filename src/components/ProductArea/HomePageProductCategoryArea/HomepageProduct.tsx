f3cdced0132075f6c580f31a2d732cd69305009f
roms <rusirusamaraweera749@gmail.com>
2024-03-17 18:50:51 +0530

    modify product category list

--- src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
+++ src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React from 'react';
 import ItemCard from '../../UI/ItemCard.tsx';
+import { Button } from 'antd';
 
 const HomepageProduct = () => {
   return (
-    <div>HomepageProduct</div>
-  )
+    <div className='product-carousel relative overflow-hidden p-6'>
+      <Button className='pre-btn rounded absolute flex top-60 bttom-auto justify-center items-center border-0 '>
+        &lt;
+      </Button>
+      <Button className='next-btn rounded absolute right-0 flex top-60 bttom-auto justify-center items-center border-0'>
+        &gt;
+      </Button>
+
+      <div className='product-container px-2.5 flex scroll-smooth overflow-hidden'>
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+        <ItemCard />
+      </div>
+    </div>
+  );
 };
 
-export default HomepageProduct
+export default HomepageProduct;
