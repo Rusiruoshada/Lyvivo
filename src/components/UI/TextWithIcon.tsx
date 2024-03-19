44dc1adfa7b407727749d4bfb290c26b161c7766
roms <rusirusamaraweera749@gmail.com>
2024-03-19 18:07:37 +0530

    modify content

--- src/components/UI/TextWithIcon.tsx
+++ src/components/UI/TextWithIcon.tsx
@@ -1,10 +1,23 @@
-import { Space } from 'antd';
-import React from 'react'
+import { Avatar, Card, Space } from 'antd';
+import React from 'react';
+import { MdOutlineLocalOffer } from "react-icons/md";
+
+const Meta = Card.Meta;
 
 const TextWithIcon = () => {
   return (
-    <Space>TextWithIcon</Space>
-  )
-}
+    <Space direction='vertical'>
+      <Card style={{ width: 300, marginTop: 16,border:0,  }}>
+        <Meta
+          avatar={
+            <Avatar icon={<MdOutlineLocalOffer />} />
+          }
+          title='Card title'
+          description='This is the description'
+        />
+      </Card>
+    </Space>
+  );
+};
 
 export default TextWithIcon;
