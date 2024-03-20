5fd9ebce0e7f0ea3ab306ebe345f0968868cd140
roms <rusirusamaraweera749@gmail.com>
2024-03-20 18:49:46 +0530

    modify text with icon component

--- src/components/UI/TextWithIcon.tsx
+++ src/components/UI/TextWithIcon.tsx
@@ -1,21 +1,61 @@
 import { Avatar, Card, Space } from 'antd';
 import React from 'react';
-import { MdOutlineLocalOffer } from "react-icons/md";
+import { MdOutlineLocalOffer } from 'react-icons/md';
+
+interface DetailsTypes {
+  title: string;
+  description: string;
+  icon: JSX.Element;
+}
 
 const Meta = Card.Meta;
 
+const details: DetailsTypes[] = [
+  {
+    title: 'Best Prices & Offers',
+    description: 'Cheaper prices than your local supermarket.',
+    icon: <MdOutlineLocalOffer />,
+  },
+  {
+    title: 'Get groceries delivered',
+    description: 'We will deliver your groceries direct to your door.',
+    icon: <MdOutlineLocalOffer />,
+  },
+  {
+    title: 'More payment Options',
+    description:
+      'Now you can Pay Online, by Cash on Delivery or Card on Delivery.',
+    icon: <MdOutlineLocalOffer />,
+  },
+  {
+    title: 'Wide Assortment',
+    description:
+      'Choose from 5000+ products across food, personal care, household & other categories.',
+    icon: <MdOutlineLocalOffer />,
+  },
+];
+
 const TextWithIcon = () => {
   return (
-    <Space direction='vertical'>
-      <Card style={{ width: 300, marginTop: 16,border:0,  }}>
-        <Meta
-          avatar={
-            <Avatar icon={<MdOutlineLocalOffer />} />
-          }
-          title='Card title'
-          description='This is the description'
-        />
-      </Card>
+    <Space
+      direction='horizontal'
+      align='start'
+      className='grid justify-center grid-cols-1 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-4 '
+    >
+      {details.map((detailItem) => (
+        <Card
+          style={{ marginTop: 16, border: 0,padding:0 }}
+          className='flex justify-center w-fit'
+          key={detailItem.title}
+          
+        >
+          <Meta
+            avatar={<Avatar icon={detailItem.icon} />}
+            title=<p className='m-0'>{detailItem.title}</p>
+            description=<p>{detailItem.description}</p>
+          />
+        </Card>
+      ))}
     </Space>
   );
 };
