1c9dfc60bf5a18efc8a777a39d04e086b18275b3
roms <rusirusamaraweera749@gmail.com>
2024-03-21 02:04:05 +0530

    modify style

--- src/components/UI/TextWithIcon.tsx
+++ src/components/UI/TextWithIcon.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, Space } from 'antd';
+import { Avatar, Card, Divider, Space } from 'antd';
 import React from 'react';
 import { MdOutlineLocalOffer } from 'react-icons/md';
 import { TbTruckDelivery } from 'react-icons/tb';
@@ -43,24 +43,24 @@ const TextWithIcon = () => {
     <Space
       direction='horizontal'
       align='start'
-      className='grid justify-center grid-cols-1 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-4 sm:my-1 lg:my-4'
+      className='grid justify-center grid-cols-1 sm:grid-cols-1  md:grid-cols-2 lg:grid-cols-4 sm:my-1 lg:my-4 sm:mx-0 md:mx-6 lg:mx-6'
     >
       {details.map((detailItem) => (
         <Card
           style={{ marginTop: 16, border: 0, padding: 0 }}
-          className='flex justify-center w-fit [&>div]:!py-0'
-          key={detailItem.title}
+          className='flex justify-center w-fit [&>div]:!py-0 '
           size='small'
+          key={detailItem.title}
         >
           <Meta
             avatar={
               <Avatar
-                className='bg-transparent text-[--secondaryColor] shadow-sm'
+                className='bg-transparent text-[--secondaryColor] shadow-sm pr-0'
                 icon={detailItem.icon}
               />
             }
-            title=<p className='m-0 text-sm'>{detailItem.title}</p>
-            description=<p className='text-xs'>{detailItem.description}</p>
+            title=<p className='m-0 text-[12px] sm:text-[12px] md:text-sm lg:text-sm'>{detailItem.title}</p>
+            description=<p className='m-0 text-[10px] sm:text-[12px] md:text-xs lg:text-xs'>{detailItem.description}</p>
           />
         </Card>
       ))}
