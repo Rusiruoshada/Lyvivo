0db567bb0dcec6d5a2d069b342a20c80dc61d9a3
roms <rusirusamaraweera749@gmail.com>
2024-03-22 20:51:35 +0530

    modify style

--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -11,18 +11,21 @@ const ItemCard: React.FC = () => {
   const cardDescription: string = '(Inclusive of all taxes)';
   const cardPrice: string = parseFloat('32.00').toFixed(2);
   const badgeRibbonText: string = 'In Stock';
-  const badgeColor = 'var(--secondaryColor)'
-
+  const badgeColor:string = 'var(--secondaryColor)'
+  const saving:string = 'Save Rs.120.00';
+  
   return (
     <Card
       style={{ width: 300 }}
       cover={
         <Badge.Ribbon text={badgeRibbonText} color={badgeColor}>
+        <Badge.Ribbon text={saving}placement='start' color='var(--savingBadgeColor)' className={`text-[18px] py-1 ${!saving? 'hidden':''}`}>
           <img
             className='aspect-square '
             alt='example'
             src='./images/Pic923056.jpg'
           />
+        </Badge.Ribbon>
         </Badge.Ribbon>
       }
       className='scale-75 hover:shadow-lg hover:scale-[0.76] border-2'
