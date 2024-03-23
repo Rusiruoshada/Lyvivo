0a666d2947160606c811df844259f04565d76adf
roms <rusirusamaraweera749@gmail.com>
2024-03-23 14:36:39 +0530

    modify style in badge for savings

--- src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
+++ src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
@@ -6,10 +6,11 @@ import { useDragScroll } from '../../../hooks/useDragScroll.ts';
 const HomepageProduct = () => {
 
   const [ref] = useDragScroll();
+  const productTitle:string = 'Lyvivo Products';
 
   return (
     <div className='product-carousel overflow-hidden relative  p-6'>
-
+      <h4>{productTitle}</h4>
       <div
         className='product-container grid grid-flow-col scroll-smooth transition-transform ease-out duration-500 justify-between overflow-x-scroll '
         ref={ref}
