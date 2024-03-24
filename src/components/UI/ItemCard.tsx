a69b689c71084ad119483d85f16af4f6171e578c
roms <rusirusamaraweera749@gmail.com>
2024-03-24 07:37:55 +0530

    modify style and data handle

--- src/components/UI/ItemCard.tsx
+++ src/components/UI/ItemCard.tsx
@@ -2,39 +2,50 @@ import React from 'react';
 import { Button, Card, Badge } from 'antd';
 import { BsCart2 } from 'react-icons/bs';
 import DropDown from './DropDown.tsx';
-import categories from '../HomeShopByCategory/exportCategoryObject.tsx';
 
 interface ItemCardProps {
   cardTitle: string;
-  cardDescription: string
-  cardPrice: string
-  badgeRibbonText?: string
-  badgeColor?:string
-  saving?:string 
+  cardDescription: string;
+  cardPrice: string;
+  badgeRibbonText?: string;
+  badgeColor?: string;
+  saving?: string;
+  categories: {
+    value: string;
+    label: string;
+    children: {value: string; label: string;}[];
+    imgPath: string;
+  }[]
 }
 
 const { Meta } = Card;
 
-const ItemCard: React.FC = () => {
-  const cardTitle: string = 'hello kitty';
-  const cardDescription: string = '(Inclusive of all taxes)';
-  const cardPrice: string = parseFloat('32.00').toFixed(2);
-  const badgeRibbonText: string = 'In Stock';
-  const badgeColor:string = 'var(--secondaryColor)'
-  const saving:string = 'Save Rs.120.00';
-
+const ItemCard: React.FC<ItemCardProps> = ({
+  cardTitle,
+  cardDescription,
+  cardPrice,
+  badgeRibbonText,
+  badgeColor,
+  saving,
+  categories
+}) => {
   return (
     <Card
       style={{ width: 300 }}
       cover={
         <Badge.Ribbon text={badgeRibbonText} color={badgeColor}>
-        <Badge.Ribbon text={saving}placement='start' color='var(--savingBadgeColor)' className={`text-[18px] py-1 ${!saving? 'hidden':''}`}>
-          <img
-            className='aspect-square '
-            alt='example'
-            src='./images/Pic923056.jpg'
-          />
-        </Badge.Ribbon>
+          <Badge.Ribbon
+            text={saving}
+            placement='start'
+            color='var(--savingBadgeColor)'
+            className={`text-[18px] py-1 ${!saving ? 'hidden' : ''}`}
+          >
+            <img
+              className='aspect-square '
+              alt='example'
+              src='./images/Pic923056.jpg'
+            />
+          </Badge.Ribbon>
         </Badge.Ribbon>
       }
       className='scale-75 hover:shadow-lg hover:scale-[0.76] border-2'
