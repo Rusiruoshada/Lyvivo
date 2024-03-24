aa35c46cfb278f3308330008e4818a053c0f33c1
roms <rusirusamaraweera749@gmail.com>
2024-03-24 20:04:13 +0530

    create navbar to contain all navbar components

---
+++ src/components/Navbars/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UpperNav from './UpperNav.tsx'
+import BottomNav from './BottomNav.tsx';
+
+const Navbar = () => {
+  return (
+    <>
+        <UpperNav />
+        <BottomNav />
+    </>
+  )
+}
+
+export default Navbar;
