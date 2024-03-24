fca6d28fed2f1e3f73859d85f0a775e60abe4a0f
roms <rusirusamaraweera749@gmail.com>
2024-03-24 07:37:30 +0530

    modify for more responsive

---
+++ src/components/ProductArea/ProductSpace.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomepageProduct from './HomePageProductCategoryArea/HomepageProduct.tsx';
+
+const ProductSpace:React.FC = () => {
+  return (
+    <div>
+      <HomepageProduct productTitle='Lyvivo Products' />
+    </div>
+  );
+};
+
+export default ProductSpace;
