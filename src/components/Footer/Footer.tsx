b34fee351390667daa5af5c2afb6ae7e81e3ad44
roms <rusirusamaraweera749@gmail.com>
2024-03-25 05:19:11 +0530

    create footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  FaInstagram,
+  FaDribbble,
+  FaXTwitter,
+  FaYoutube,
+} from 'react-icons/fa6';
+
+const Footer: React.FC = () => {
+  const socialLinks = [
+    { label: 'YouTube', icon: FaYoutube },
+    { label: 'Instagram', icon: FaInstagram },
+    { label: 'Twitter', icon: FaXTwitter },
+    { label: 'Dribbble', icon: FaDribbble },
+  ];
+
+
+  return (
+    <div className=''>
+
+    </div>
+  );
+};
+
+export default Footer;
