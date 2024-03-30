8ed400112fa0cfda234bcd66ae071fe579faf01e
roms <rusirusamaraweera749@gmail.com>
2024-03-30 16:02:11 +0530

    create separate file for middle footer links

---
+++ src/components/Footer/middleFooterLinks.tsx
@@ -0,0 +1,51 @@
+import { Typography } from "antd";
+import React from "react";
+
+interface MiddleFooterLinks {
+  key: string;
+  label: string;
+  children: React.JSX.Element[];
+}
+
+const { Text } = Typography;
+
+const links:MiddleFooterLinks[] = [
+    {
+      key: '1',
+      label: 'Quick Links',
+      children: [
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500 '>Home</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Catalogue & Deals</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Track my order</Text>,
+      ],
+    },
+    {
+      key: '2',
+      label: 'Categories',
+      children: [
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Grocery</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Pharmacy</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Food</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Electronics</Text>,
+      ],
+    },
+    {
+      key: '3',
+      label: 'Quick Links',
+      children: [
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Privacy Policy</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >FAQ</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Terms and Conditions</Text>,
+      ],
+    },
+    {
+      key: '4',
+      label: 'Customer Service',
+      children: [
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Contact us</Text>,
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >About us</Text>,
+      ],
+    },
+  ];
+
+  export default links;
