2112046d7a19199d1c0fe2a1f6eaeed43d94ce84
roms <rusirusamaraweera749@gmail.com>
2024-04-01 14:46:54 +0530

    modify logo for link and set styles

--- src/components/Footer/UpperFooter.tsx
+++ src/components/Footer/UpperFooter.tsx
@@ -4,14 +4,12 @@ import { FaFacebookF, FaTwitter } from 'react-icons/fa';
 import { RiInstagramFill } from 'react-icons/ri';
 
 const UpperFooter: React.FC = () => {
-  const { Title } = Typography;
+  const { Title,Link } = Typography;
 
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-1 md:!grid-cols-4 lg:!grid-cols-6 items-center gap-y-4 sm:gap-y-4 md:!gap-y-0 lg:!gap-y-0 '>
+    <div className='grid grid-cols-1 sm:!grid-cols-1 md:!grid-cols-3 lg:!grid-cols-6 items-center gap-y-4 sm:gap-y-4 md:!gap-y-6 lg:!gap-y-0 '>
       <div className='flex justify-center sm:justify-center md:!justify-start lg:!justify-start'>
-        <Title level={1} className='font-mono font-bold text-white mb-0'>
-          Lyvivo
-        </Title>
+          <Link href='/' className='font-mono font-bold text-white mb-0 text-4xl'>Lyvivo</Link>
       </div>
       <Space.Compact className='col-span-1 md:!col-span-2 lg:!col-span-4 mx-2' size='middle'>
         <Input
@@ -28,7 +26,7 @@ const UpperFooter: React.FC = () => {
       </Space.Compact>
       <Space
         size='large'
-        className='flex justify-center sm:justify-center md:!justify-end lg:!justify-end gap-2 '
+        className='flex justify-center sm:!justify-center md:!justify-center lg:!justify-end gap-2 sm:!col-span-3 md:!col-span-3 lg:!col-span-1'
       >
         <Button className='hover:!bg-[var(--primaryColor)] border-[var(--primaryColor)]'>
           <FaFacebookF className='text-white ' />
