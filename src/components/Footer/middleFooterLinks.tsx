644e7509620ca0f6339bf726eff0d28e7fb5367f
roms <rusirusamaraweera749@gmail.com>
2024-04-01 13:20:36 +0530

    change structure of children data

--- src/components/Footer/middleFooterLinks.tsx
+++ src/components/Footer/middleFooterLinks.tsx
@@ -4,7 +4,7 @@ import React from "react";
 interface MiddleFooterLinks {
   key: string;
   label: string;
-  children: React.JSX.Element[];
+  children: React.JSX.Element;
 }
 
 const { Text } = Typography;
@@ -13,38 +13,38 @@ const links:MiddleFooterLinks[] = [
     {
       key: '1',
       label: 'Quick Links',
-      children: [
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500 '>Home</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Catalogue & Deals</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Track my order</Text>,
-      ],
+      children: <div className='flex flex-col'>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500 '>Home</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Catalogue & Deals</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Track my order</Text>
+      </div>
     },
     {
       key: '2',
       label: 'Categories',
-      children: [
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Grocery</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Pharmacy</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Food</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Electronics</Text>,
-      ],
+      children: <div className='flex flex-col'>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Grocery</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Pharmacy</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Food</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Electronics</Text>
+        </div>
     },
     {
       key: '3',
       label: 'Useful Links',
-      children: [
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Privacy Policy</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >FAQ</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Terms and Conditions</Text>,
-      ],
+      children: <div className='flex flex-col'>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Privacy Policy</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >FAQ</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Terms and Conditions</Text>
+        </div>
     },
     {
       key: '4',
       label: 'Customer Service',
-      children: [
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Contact us</Text>,
-        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >About us</Text>,
-      ],
+      children: <div className='flex flex-col'>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >Contact us</Text>
+        <Text className='text-white hover:text-[var(--accentColor)] hover:translate-x-1 ease-in-out duration-500' >About us</Text>
+        </div>
     },
   ];
 
