297e87422bf6a7bbf2582203f025c5250115f792
roms <rusirusamaraweera749@gmail.com>
2024-04-08 15:16:37 +0530

    modify with react multi carousel library

--- src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
+++ src/components/ProductArea/HomePageProductCategoryArea/HomepageProduct.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ItemCard from '../../UI/ItemCard.tsx';
 import { useDragScroll } from '../../../hooks/useDragScroll.ts';
 import categories from '../../HomeShopByCategory/exportCategoryObject.tsx';
+import Carousel from 'react-multi-carousel';
+import responsive from './responsive.ts';
 
 interface HomepageProductProps {
   productTitle: string;
@@ -19,59 +21,83 @@ const HomepageProduct: React.FC<HomepageProductProps> = ({ productTitle }) => {
   const [ref] = useDragScroll();
 
   return (
-    <div className='product-carousel overflow-hidden relative  py-6 px-10 '>
-      <h4>{productTitle}</h4>
-      <div
-        className='product-container grid grid-flow-col scroll-smooth transition-transform ease-out duration-500 justify-between overflow-x-scroll '
-        ref={ref}
-      >
-        <ItemCard
-          cardTitle={cardTitle}
-          cardDescription={cardDescription}
-          cardPrice={cardPrice}
-          badgeRibbonText={badgeRibbonText}
-          badgeColor={badgeColor}
-          saving={saving}
-          categories={categories}
-        />
-        <ItemCard
-          cardTitle={cardTitle}
-          cardDescription={cardDescription}
-          cardPrice={cardPrice}
-          badgeRibbonText={badgeRibbonText}
-          badgeColor={badgeColor}
-          saving={saving}
-          categories={categories}
-        />
-        <ItemCard
-          cardTitle={cardTitle}
-          cardDescription={cardDescription}
-          cardPrice={cardPrice}
-          badgeRibbonText={badgeRibbonText}
-          badgeColor={badgeColor}
-          saving={saving}
-          categories={categories}
-        />
-        <ItemCard
-          cardTitle={cardTitle}
-          cardDescription={cardDescription}
-          cardPrice={cardPrice}
-          badgeRibbonText={badgeRibbonText}
-          badgeColor={badgeColor}
-          saving={saving}
-          categories={categories}
-        />
-        <ItemCard
-          cardTitle={cardTitle}
-          cardDescription={cardDescription}
-          cardPrice={cardPrice}
-          badgeRibbonText={badgeRibbonText}
-          badgeColor={badgeColor}
-          saving={saving}
-          categories={categories}
-        />
+    // <div className='product-carousel overflow-hidden relative  py-6 '>
+    //   <h4>{productTitle}</h4>
+    //   <div
+    //     className='product-container grid grid-flow-col scroll-smooth transition-transform ease-out duration-500 justify-between overflow-x-scroll '
+    //     ref={ref}
+    //   >
+    //     <ItemCard
+    //       cardTitle={cardTitle}
+    //       cardDescription={cardDescription}
+    //       cardPrice={cardPrice}
+    //       badgeRibbonText={badgeRibbonText}
+    //       badgeColor={badgeColor}
+    //       saving={saving}
+    //       categories={categories}
+    //     />
+    //     <ItemCard
+    //       cardTitle={cardTitle}
+    //       cardDescription={cardDescription}
+    //       cardPrice={cardPrice}
+    //       badgeRibbonText={badgeRibbonText}
+    //       badgeColor={badgeColor}
+    //       saving={saving}
+    //       categories={categories}
+    //     />
+    //     <ItemCard
+    //       cardTitle={cardTitle}
+    //       cardDescription={cardDescription}
+    //       cardPrice={cardPrice}
+    //       badgeRibbonText={badgeRibbonText}
+    //       badgeColor={badgeColor}
+    //       saving={saving}
+    //       categories={categories}
+    //     />
+    //     <ItemCard
+    //       cardTitle={cardTitle}
+    //       cardDescription={cardDescription}
+    //       cardPrice={cardPrice}
+    //       badgeRibbonText={badgeRibbonText}
+    //       badgeColor={badgeColor}
+    //       saving={saving}
+    //       categories={categories}
+    //     />
+    //     <ItemCard
+    //       cardTitle={cardTitle}
+    //       cardDescription={cardDescription}
+    //       cardPrice={cardPrice}
+    //       badgeRibbonText={badgeRibbonText}
+    //       badgeColor={badgeColor}
+    //       saving={saving}
+    //       categories={categories}
+    //     />
 
-      </div>
+    //   </div>
+    // </div>
+    <div className="parent">
+      <Carousel
+        responsive={responsive}
+        autoPlay={true}
+        swipeable={true}
+        draggable={true}
+        showDots={true}
+        infinite={true}
+        partialVisible={false}
+        dotListClass="custom-dot-list-style"
+      >
+        {[1,2,3,4,5].map((imageUrl, index) => (
+            <ItemCard 
+              cardTitle={cardTitle}
+              cardDescription={cardDescription}
+              cardPrice={cardPrice}
+              badgeRibbonText={badgeRibbonText}
+              key={index} 
+              categories={categories} 
+            />
+          )
+        )}
+      </Carousel>
     </div>
   );
 };
