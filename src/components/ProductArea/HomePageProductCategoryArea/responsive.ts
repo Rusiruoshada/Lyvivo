f7594f8188ec4ba1327e63306666c98ab25aafcb
roms <rusirusamaraweera749@gmail.com>
2024-04-11 15:52:59 +0530

    add partial visibility

--- src/components/ProductArea/HomePageProductCategoryArea/responsive.ts
+++ src/components/ProductArea/HomePageProductCategoryArea/responsive.ts
@@ -21,24 +21,28 @@ interface ResponsiveTypes {
 
 const responsive = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 1025 },
     items: 4,
     slidesToSlide: 4 // optional, default to 1.
   },
   laptop: {
     breakpoint: { max: 1024, min: 769 },
-    items: 4,
-    slidesToSlide: 4, // optional, default to 1.
+    items: 3,
+    slidesToSlide: 3, // optional, default to 1.
+    
   },
   tablet: {
     breakpoint: { max: 768, min: 465 },
-    items: 3,
-    slidesToSlide: 3 // optional, default to 1.
+    items: 2,
+    slidesToSlide: 2, // optional, default to 1.
+    partialVisibilityGutter: -20,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1 // optional, default to 1.
+    items: 2,
+    slidesToSlide: 1, // optional, default to 1.
+    partialVisibilityGutter: -100,
+    
   }
   };
 
