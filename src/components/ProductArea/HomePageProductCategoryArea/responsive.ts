ee823bf0a7f21e9d87eb9b12edb890d7a68c1ff9
roms <rusirusamaraweera749@gmail.com>
2024-04-11 12:38:01 +0530

    change structure

--- src/components/ProductArea/HomePageProductCategoryArea/responsive.ts
+++ src/components/ProductArea/HomePageProductCategoryArea/responsive.ts
@@ -1,37 +1,45 @@
 interface ResponsiveTypes {
   desktop:{
-    breakpoint: {max:number, min:number}
+    breakpoint: {max:number, min:number},
     items: number,
-    slidesToSlide: number
+    slidesToSlide: number,
+    
   },
   tablet:{
-    breakpoint: {max:number, min:number}
+    breakpoint: {max:number, min:number},
     items: number,
-    slidesToSlide: number
+    slidesToSlide: number,
+    
   },
   mobile:{
-    breakpoint: {max:number, min:number}
+    breakpoint: {max:number, min:number},
     items: number,
-    slidesToSlide: number
+    slidesToSlide: number,
+    
   },
 }
 
 const responsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 4,
-      slidesToSlide: 4 // optional, default to 1.
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 768 },
-      items: 3,
-      slidesToSlide: 3 // optional, default to 1.
-    },
-    mobile: {
-      breakpoint: { max: 767, min: 464 },
-      items: 2,
-      slidesToSlide: 1 // optional, default to 1.
-    }
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 4 // optional, default to 1.
+  },
+  laptop: {
+    breakpoint: { max: 1024, min: 769 },
+    items: 4,
+    slidesToSlide: 4, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 465 },
+    items: 3,
+    slidesToSlide: 3 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
+  }
   };
 
   export default responsive;
