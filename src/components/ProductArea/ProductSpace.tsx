672b25ca88eefbf81e4dcc4afc0e614556e26ee7
roms <rusirusamaraweera749@gmail.com>
2024-04-15 17:28:11 +0530

    modify product space

--- src/components/ProductArea/ProductSpace.tsx
+++ src/components/ProductArea/ProductSpace.tsx
@@ -4,6 +4,8 @@ import HomepageProduct from './HomePageProductCategoryArea/HomepageProduct.tsx';
 const ProductSpace:React.FC = () => {
   return (
     <div>
+      <HomepageProduct productTitle='Best Savings' />
+      <HomepageProduct productTitle='Groceries' />
       <HomepageProduct productTitle='Lyvivo Products' />
     </div>
   );
