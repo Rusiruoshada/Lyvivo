5d57fc2befb4d2ad49ac2458790eb15edb32dcb6
roms <rusirusamaraweera749@gmail.com>
2024-04-20 23:06:38 +0530

    modify category space

--- src/components/HomeShopByCategory/CategorySpace.tsx
+++ src/components/HomeShopByCategory/CategorySpace.tsx
@@ -21,14 +21,14 @@ const CategorySpace: React.FC = () => (
     <Row gutter={20} justify='center' className='p-0 gap-3'>
       {categories1.map((categoryItem) => (
         <Col
-          className='py-3 sm:py-2 md:p-5 lg:p-10 px-0 scale-100 sm:scale-100 md:scale-75 lg:scale-75'
+          className='py-0 sm:py-2 md:p-5 lg:p-10 px-0 scale-100 sm:scale-100 md:scale-75 lg:scale-75 shadow-md'
           xs={6}
           sm={4}
           md={4}
           lg={4}
           key={categoryItem.label}
         >
-          <Card.Grid style={gridStyle} className='p-0 h-44 items-center flex shadow-none'>
+          <Card.Grid style={gridStyle} className='p-0 h-fit items-center flex shadow-none'>
             <CategoryCard
               categoryName={categoryItem.value}
               categoryImg={categoryItem.imgPath}
