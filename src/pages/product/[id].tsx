3c71a09461b0cabfaaeb421471a28cf4bf32f7a6
roms <rusirusamaraweera749@gmail.com>
2024-04-27 18:14:24 +0530

    change style

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import Images from './Image.tsx';
+import Description from './Description.jsx';
+
+interface ImageType {
+    thumbnail: string,
+    fullSize: string, 
+    alt: string
+}
+
+const images:ImageType[] = [
+    {
+      thumbnail: '/Pic923056.jpg',
+      fullSize: '/images/Pic923056.jpg',
+      alt:'1'
+    },
+    {
+      thumbnail: '/slide1.png',
+      fullSize: '/images/slide1.png',
+      alt:'2'
+    },
+    {
+      thumbnail: '/slide2.png',
+      fullSize: '/images/slide2.png',
+      alt:'3'
+    },
+    {
+      thumbnail: '/slide3.jpg',
+      fullSize: '/images/slide3.jpg',
+      alt:'4'
+    },
+  ];
 
 const SingleProductPage = () => {
   return (
-    <div>
-        <Images />
+    <div className="flex items-center gap-16 px-10 py-20 max-lg:flex-col max-sm:py-0 max-sm:px-0 mb-10">
+        <Images images={images} />
+        <Description />
     </div>
 )};
 
