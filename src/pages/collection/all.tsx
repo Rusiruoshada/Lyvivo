b9a76a11c03245edff0b51ae61c2557fc6303c07
roms <rusirusamaraweera749@gmail.com>
2024-05-11 15:33:31 +0530

    modify all page

--- src/pages/collection/all.tsx
+++ src/pages/collection/all.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Path from '../../components/Breadcrumb/Breadcrumb.tsx';
+import Filter from '../../components/ProductArea/AllProduct/FilterSection/Filter.tsx';
 
 function AllProduct() {
   return (
     <>
       <Path />
-    
+      <div>
+        <Filter count={[30,15,3,200,3]} />
+      </div>
     </>
   )
 }
