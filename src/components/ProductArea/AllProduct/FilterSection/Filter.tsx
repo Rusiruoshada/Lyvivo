a87a0ae894520711b34d9d2e4f2a25d8c8f1987b
roms <rusirusamaraweera749@gmail.com>
2024-05-21 23:49:27 +0530

    modify filter

--- src/components/ProductArea/AllProduct/FilterSection/Filter.tsx
+++ src/components/ProductArea/AllProduct/FilterSection/Filter.tsx
@@ -1,4 +1,4 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import filters from './filterContent.ts';
 import { Button, Collapse, Divider, InputNumber, Slider } from 'antd';
 
@@ -6,21 +6,21 @@ interface FilterProps {
   count: number[];
 }
 
-interface Items{
-  key: string,
-  label: JSX.Element,
+interface Items {
+  key: string;
+  label: JSX.Element;
   children: JSX.Element[];
 }
 
-interface Price{
-  key: string,
-  label: JSX.Element,
+interface Price {
+  key: string;
+  label: JSX.Element;
   children: JSX.Element;
 }
 
-interface Availability{
-  key: string,
-  label: JSX.Element,
+interface Availability {
+  key: string;
+  label: JSX.Element;
   children: JSX.Element;
 }
 
@@ -29,33 +29,31 @@ const Filter: React.FC<FilterProps> = ({ count }) => {
 
   const onChangeInput: any = (newValue: number) => {
     setInputValue((prev) =>
-    prev.input1 !== newValue[0]
-    ? { input1: newValue[0], input2: prev.input2 }
-    : { input1: prev.input1, input2: newValue[1] }
+      prev.input1 !== newValue[0]
+        ? { input1: newValue[0], input2: prev.input2 }
+        : { input1: prev.input1, input2: newValue[1] }
     );
   };
-  
-  const items:Items[] = [
+
+  const items: Items[] = [
     {
       key: '1',
       label: <p className='font-bold my-0 py-0'>Product Types</p>,
-      children: 
-        filters.productType.map((productItem, index) => (
-          <div key={index} className='my-0 py-0 flex gap-2'>
-            <input type='checkbox' />{' '}
-            <label title={productItem.label}>{productItem.label}</label>
-          </div>
-        )),
-      
+      children: filters.productType.map((productItem, index) => (
+        <div key={index} className='my-0 py-0 flex gap-2'>
+          <input type='checkbox' />{' '}
+          <label title={productItem.label}>{productItem.label}</label>
+        </div>
+      )),
     },
   ];
-  
-  const price:Price[] = [
+
+  const price: Price[] = [
     {
       key: '2',
       label: <p className='font-bold py-0 my-0'>Price</p>,
-      children: 
-        <div className='py-0' style={{padding:0}}>
+      children: (
+        <div className='py-0' style={{ padding: 0 }}>
           <Slider
             min={1}
             max={9999}
@@ -84,27 +82,26 @@ const Filter: React.FC<FilterProps> = ({ count }) => {
             />
           </div>
         </div>
-    }
-  ]
-  
-  const availability:Availability[] = [
+      ),
+    },
+  ];
+
+  const availability: Availability[] = [
     {
       key: '3',
       label: <p className='font-bold my-0 py-0'>Availability</p>,
-      children: 
-          <div className='my-0 py-0'>
-            <div className='flex gap-2'>
-              <input title='In stock' type='checkbox' />{' '}
-              <label  title={'In stock'}>{'In stock'}</label>
+      children: (
+        <div className='my-0 py-0'>
+          {filters.availability.map((available, index) => (
+            <div className='flex gap-2' key={available + index}>
+              <input title={available} type='checkbox' />{' '}
+              <label title={available}>{available} </label>
             </div>
-            <div className='flex gap-2'>
-              <input title='Out of stock' type='checkbox' />{' '}
-              <label  title={'Out of stock'}>{'Out of stock'}</label>
-            </div>
-          </div>
-      
+          ))}
+        </div>
+      ),
     },
-  ]
+  ];
 
   return (
     <section className='border border-gray-300 w-fit mx-10'>
@@ -123,7 +120,7 @@ const Filter: React.FC<FilterProps> = ({ count }) => {
           </Button>
         ))}
       </section>
-      <Divider />
+      <Divider style={{margin:'1rem 0'}} />
       <section className='px-3'>
         <h4>Filters</h4>
         <Collapse
@@ -139,27 +136,27 @@ const Filter: React.FC<FilterProps> = ({ count }) => {
       </section>
       <section className='px-3'>
         <Collapse
-            // onClick={onClick}
-            style={{ width: 256, borderBottom: 'none' }}
-            items={price}
-            expandIconPosition='end'
-            bordered={false}
-            ghost
-            defaultActiveKey={['2']}
-            className='border-none shadow-none py-0'
-          />
+          // onClick={onClick}
+          style={{ width: 256, borderBottom: 'none' }}
+          items={price}
+          expandIconPosition='end'
+          bordered={false}
+          ghost
+          defaultActiveKey={['2']}
+          className='border-none shadow-none py-0'
+        />
       </section>
       <section className='px-3'>
         <Collapse
-            // onClick={onClick}
-            style={{ width: 256, borderBottom: 'none' }}
-            items={availability}
-            expandIconPosition='end'
-            bordered={false}
-            ghost
-            defaultActiveKey={['3']}
-            className='border-none shadow-none py-0'
-          />
+          // onClick={onClick}
+          style={{ width: 256, borderBottom: 'none' }}
+          items={availability}
+          expandIconPosition='end'
+          bordered={false}
+          ghost
+          defaultActiveKey={['3']}
+          className='border-none shadow-none py-0'
+        />
       </section>
     </section>
   );
