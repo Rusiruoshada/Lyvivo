87c05a6c2222064cdff2f1f5e4e57c3426695ec7
roms <rusirusamaraweera749@gmail.com>
2024-05-28 22:31:47 +0530

    change store

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,4 +6,10 @@ const store = configureStore({
     }
 });
 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+//0717312175
+
 export default store;
