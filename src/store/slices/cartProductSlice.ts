4a6c0a2ed37e103ede15be9545725c8b00199706
roms <rusirusamaraweera749@gmail.com>
2024-06-06 22:38:00 +0530

    create cartProductSlice

---
+++ src/store/slices/cartProductSlice.ts
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartProductsInitialState = {
+    cartProducts: [],
+}
+
+const cartProductSlice = createSlice({
+    name:'cartProduct',
+    initialState: cartProductsInitialState,
+    reducers: {
+
+    }
+});
+
+export const cartProduct = cartProductSlice.actions;
+
+export default cartProductSlice.reducer;
