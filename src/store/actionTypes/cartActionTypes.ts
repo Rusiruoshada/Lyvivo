fb001f01061a7282142e8d938d5952141f3de8d4
roms <rusirusamaraweera749@gmail.com>
2024-07-17 13:48:04 +0530

    add new types to productDetails

--- src/store/actionTypes/cartActionTypes.ts
+++ src/store/actionTypes/cartActionTypes.ts
@@ -1,13 +1,20 @@
 export interface CartProductsInitialState {
-    cartProducts?: {id:string}[],
-    productCount?: number,
-    cartProductCount?: number,
-    totalPriceForProduct? : number,
+  cartProducts?: { id: string }[];
+  productCount?: number;
+  cartProductCount?: number;
+  totalPriceForProduct?: number;
+  cartProductDetails?: {
+    id: string;
+    productName: string;
+    image: string;
+    addItems: number;
+    price: number;
+  }[];
 }
 
 export interface addProductAction {
-    type: string,
-    payload: CartProductsInitialState
+  type: string;
+  payload: CartProductsInitialState;
 }
 
 export type CartActionTypes = CartProductsInitialState | addProductAction;
