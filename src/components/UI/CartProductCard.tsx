1ad099222b6a1889e00e4977aa17f9ee9e4db44e
roms <rusirusamaraweera749@gmail.com>
2024-07-26 23:11:24 +0530

    add countdown

--- src/components/UI/CartProductCard.tsx
+++ src/components/UI/CartProductCard.tsx
@@ -18,6 +18,9 @@ const CartProductCard: React.FC<CartProductCardProps> = ({
   image,
   removeProductFC,
 }) => {
+
+  const isMinusShow:boolean = count <= 1;
+
   return (
     <div className='shadow-md p-2'>
       <Flex gap='middle' align='start' className='gap-1' >
@@ -29,19 +32,20 @@ const CartProductCard: React.FC<CartProductCardProps> = ({
             <h6>{productName}</h6>
             <p>Rs. {price}</p>
           </div>
-          <div>
-            <FaMinus
-                className='cursor-pointer w-4 text-3xl'
-                // onClick={minus}
-            />
-            
-            <div className='font-bold text-md'>{count}</div>
-
+          <div className='mr-2 ml-2 shadow-md pr-2 pl-2 pt-0 pb-0'>
             <FaPlus className='cursor-pointer w-4 text-3xl' 
             // onClick={add}
              />
+            
+            <div className='font-bold text-md'>{count}</div>
+             
+            <FaMinus
+                className={`cursor-pointer w-4 text-3xl ${isMinusShow?'text-gray-500':''}`}
+                
+                // onClick={minus}
+            />
           </div>
-          <div className='flex justify-center align-middle'><IoIosCloseCircleOutline className='flex justify-center align-middle text-2xl' /></div>
+          <div className='flex justify-center align-middle'><IoIosCloseCircleOutline className='flex justify-center align-middle text-2xl  m-auto' /></div>
         </div>
       </Flex>
     </div>
