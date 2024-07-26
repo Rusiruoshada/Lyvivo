d4f6db12f40d9d252e5c542601d1b90a574634e3
roms <rusirusamaraweera749@gmail.com>
2024-07-26 22:59:13 +0530

    modify cartProductCards

--- src/components/UI/CartProductCard.tsx
+++ src/components/UI/CartProductCard.tsx
@@ -1,5 +1,7 @@
 import { Flex, Image } from 'antd';
 import React from 'react';
+import { FaMinus, FaPlus } from 'react-icons/fa';
+import { IoIosCloseCircleOutline } from "react-icons/io";
 
 interface CartProductCardProps {
   productName: string;
@@ -17,13 +19,31 @@ const CartProductCard: React.FC<CartProductCardProps> = ({
   removeProductFC,
 }) => {
   return (
-    <div className='shadow-md'>
-        <Flex gap='middle' align='start' vertical>
-        <div><Image width={71} src={image} /></div>
+    <div className='shadow-md p-2'>
+      <Flex gap='middle' align='start' className='gap-1' >
         <div>
-            
+          <Image width={71} src={image} />
         </div>
-        </Flex>
+        <div className='flex flex-row'>
+          <div className='flex flex-col'>
+            <h6>{productName}</h6>
+            <p>Rs. {price}</p>
+          </div>
+          <div>
+            <FaMinus
+                className='cursor-pointer w-4 text-3xl'
+                // onClick={minus}
+            />
+            
+            <div className='font-bold text-md'>{count}</div>
+
+            <FaPlus className='cursor-pointer w-4 text-3xl' 
+            // onClick={add}
+             />
+          </div>
+          <div className='flex justify-center align-middle'><IoIosCloseCircleOutline className='flex justify-center align-middle text-2xl' /></div>
+        </div>
+      </Flex>
     </div>
   );
 };
