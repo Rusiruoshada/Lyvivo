82a2ae828b2dea8cef3073137cbd9f581b2a3015
roms <rusirusamaraweera749@gmail.com>
2024-08-02 21:49:08 +0530

    router maybe old Have to change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import ClientApp from './components/ClientApp.tsx';
 import store from './store/index.ts';
 import Navbar from './components/Navbars/Navbar.tsx';
 import Footer from './components/Footer/Footer.tsx';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom';
 import SingleProductPage from './pages/product/[id].tsx';
+import AllProduct from './pages/collection/all.tsx';
+
 
 function App() {
   return (
@@ -15,7 +17,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<ClientApp />} />
-            <Route path={`/product/:id`} element={<SingleProductPage />} />
+            <Route path='/collection/all' element={<AllProduct />} />
+            <Route path='/product/:id' element={<SingleProductPage />} />
             <Route path='*'  />
           </Routes>
         </BrowserRouter>
