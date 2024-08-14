92f3cf37f99e8900a86a46650274afefaad65a97
roms <rusirusamaraweera749@gmail.com>
2024-08-14 16:09:32 +0530

    add useNavigate to better navigation

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,30 +1,47 @@
 import React from 'react';
 import { HomeOutlined } from '@ant-design/icons';
 import { Breadcrumb } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 interface BreadcrumbProps{
     currentPath?:string,
 }
 
-const Path: React.FC<BreadcrumbProps> = ({currentPath}) => (
+const Path: React.FC<BreadcrumbProps> = ({currentPath}) => {
 
-  <Breadcrumb
-        items={[
-            {
-                href: '/',
-                title: <HomeOutlined />,
-            },
-            {
-                href: '/collection/all',
-                title: <span>All Product</span>
-            },
-            {
-                title: <span>{currentPath}</span>,
-            },
-        ]}
-        separator='>'
-        className='mx-10 my-[40px]'
-    />
-);
+const navigate = useNavigate();
+
+  const handleBreadcrumbClick = (path) => {
+    navigate(path);
+  };
+
+  const breadcrumbItems = [
+    {
+        title: <HomeOutlined />,
+        path: '/',
+    },
+    {
+        path: '/collection/all',
+        title: <span>All Product</span>
+    },
+    {
+        title: <span>{currentPath}</span>,
+    },
+  ];
+
+
+  return (
+    <Breadcrumb className='mx-10 my-[40px]' separator='>'>
+      {breadcrumbItems.map((item, index) => (
+        <Breadcrumb.Item
+          key={index}
+          onClick={() => handleBreadcrumbClick(item.path)}
+        >
+          {item.title}
+        </Breadcrumb.Item>
+      ))}
+    </Breadcrumb>
+  );
+}
 
 export default Path;
