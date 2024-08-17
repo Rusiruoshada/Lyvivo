56c4570796714e06ffc7e0969db901f5d5d13e28
roms <rusirusamaraweera749@gmail.com>
2024-08-17 21:33:04 +0530

    create about page

---
+++ src/pages/aboutUs/AboutUs.tsx
@@ -0,0 +1,16 @@
+import { Image } from 'antd';
+import React from 'react'
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <img src='images/about page/about page banner.png'  alt='about banner' />
+      <div className='mx-10'>
+        <h3>Welcome to Lyvivo</h3>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutUsPage;
