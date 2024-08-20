b5409499cd3ec0ba039a9925b645f696a0407f8e
roms <rusirusamaraweera749@gmail.com>
2024-08-20 20:20:03 +0530

    modify contact page

--- src/pages/contactUs/ContactUs.tsx
+++ src/pages/contactUs/ContactUs.tsx
@@ -5,7 +5,7 @@ import { MdMail } from 'react-icons/md';
 import { RiPhoneFill } from 'react-icons/ri';
 import ContactPageForm from './ContactPageForm.tsx';
 
-const ContactUs = () => {
+const ContactUs:React.FC = () => {
   return (
     <div>
       <div className='flex relative'>
@@ -30,12 +30,12 @@ const ContactUs = () => {
           assistance, our Customer Care team is here to assist you (Refer below
           for other ways to contact us)
         </p>
-        <div className='lg:grid flex flex-col grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-7  text-white gird-rows-4 sm:grid-rows-4 md:grid-rows-4 lg:grid-rows-1 gap-1 [&_p]:m-0 '>
-          <div className='bg-green-600  col-span-3 flex lg:flex-row flex-col gap-4 p-3 lg:justify-evenly rounded-tr-[50px] lg:!rounded-bl-[50px] lg:!rounded-tr-none shadow-md'>
+        <div className='md:grid lg:grid flex flex-col grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-7  text-white gird-rows-4 sm:grid-rows-4 md:grid-rows-2 lg:grid-rows-1 gap-1 [&_p]:m-0 '>
+          <div className='bg-green-600 md:col-span-1 lg:col-span-3 flex lg:flex-row flex-col gap-4 p-3 lg:justify-evenly rounded-tr-[50px] md:!rounded-tl-[50px] md:!rounded-tr-none lg:!rounded-bl-[50px] lg:!rounded-tr-none lg:!rounded-tl-none shadow-md'>
             <h1 className='font-bold text-[48px] items-center justify-center flex m-0'>
               Lyvivo
             </h1>
-            <div className='flex flex-col justify-center text-center lg:!text-start'>
+            <div className='flex flex-col justify-center !text-center lg:!text-start'>
               <p className='m-0'>Jaykay Marketing Services Pvt Ltd</p>
               <p className='m-0'>No:148, Vauxhall Street,</p>
               <p className='m-0'>Colombo 2, Sri Lanka.</p>
@@ -47,7 +47,7 @@ const ContactUs = () => {
             </div>
             <p className='text-[12px]'>+94 11 230 3500</p>
           </div>
-          <div className='bg-green-600  flex justify-center items-center flex-col gap-3 p-3 col-span-2'>
+          <div className='bg-green-600  flex justify-center items-center flex-col gap-3 p-3 md:col-span-1 lg:col-span-2'>
             <div className='rounded-full bg-white text-2xl w-fit shadow-md p-2 '>
               <MdMail className='text-green-600' />
             </div>
@@ -55,7 +55,7 @@ const ContactUs = () => {
               lyvivocustomercare.jms@lyvivo.com
             </p>
           </div>
-          <div className='bg-green-600  flex justify-center items-center flex-col gap-3 p-3 rounded-bl-[50px] lg:!rounded-tr-[50px] lg:!rounded-bl-none'>
+          <div className='bg-green-600  flex justify-center items-center flex-col gap-3 p-3 rounded-bl-[50px] md:!rounded-bl-none md:!rounded-br-[50px] lg:!rounded-tr-[50px] lg:!rounded-bl-none lg:!rounded-br-none'>
             <div className='rounded-full bg-white text-2xl w-fit shadow-md p-2 '>
               <FaFax className='text-green-600' />
             </div>
