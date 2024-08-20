8062e29df034c5e5a077b9523b8837ec127fe206
roms <rusirusamaraweera749@gmail.com>
2024-08-20 20:18:57 +0530

    remove unnecessary inputs

--- src/pages/contactUs/ContactPageForm.tsx
+++ src/pages/contactUs/ContactPageForm.tsx
@@ -1,74 +1,71 @@
 import React, { useState } from 'react';
 import {
-  AutoComplete,
   Button,
-  Checkbox,
-  Col,
   Form,
   Input,
-  Row,
-  Select,
+  InputNumber,
 } from 'antd';
 
 const ContactPageForm:React.FC = () => {
   
-  const { Option } = Select;
 
   const formItemLayout = {
     labelCol: {
       xs: { span: 24 },
-      sm: { span: 8 },
+      sm: { span: 10 },
     },
     wrapperCol: {
       xs: { span: 24 },
-      sm: { span: 16 },
+      sm: { span: 24 },
     },
   };
-  
-  const tailFormItemLayout = {
-    wrapperCol: {
-      xs: {
-        span: 24,
-        offset: 0,
-      },
-      sm: {
-        span: 16,
-        offset: 8,
-      },
-    },
-  };
-  
+    
     const [form] = Form.useForm();
   
     const onFinish = (values: any) => {
       console.log('Received values of form: ', values);
     };
-  
-    const [autoCompleteResult, setAutoCompleteResult] = useState<string[]>([]);
-  
-    const onWebsiteChange = (value: string) => {
-      if (!value) {
-        setAutoCompleteResult([]);
-      } else {
-        setAutoCompleteResult(['.com', '.org', '.net'].map((domain) => `${value}${domain}`));
-      }
-    };
-  
-    const websiteOptions = autoCompleteResult.map((website) => ({
-      label: website,
-      value: website,
-    }));
-
+    
   return (
     <Form
       {...formItemLayout}
+      layout='vertical'
       form={form}
       name="register"
       onFinish={onFinish}
-      initialValues={{ residence: ['zhejiang', 'hangzhou', 'xihu'], prefix: '86' }}
-      style={{ maxWidth: 600 }}
       scrollToFirstError
+      
     >
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-x-3 '>
+      <Form.Item
+        name="First Name"
+        label="First Name"
+        rules={[{ required: true, message: 'Please input your First Name!', whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="Last Name"
+        label="Last Name"
+        rules={[{ required: true, message: 'Please input your Last Name!', whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="Address"
+        label="Address"
+        rules={[{ required: true, message: 'Please input your Address!', whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="Contact Number"
+        label="Contact Number"
+        rules={[{ required: true, message: 'Please input your Contact Number!', whitespace: true }]}
+      >
+        <InputNumber className='w-full' />
+      </Form.Item>
+
       <Form.Item
         name="email"
         label="E-mail"
@@ -85,118 +82,21 @@ const ContactPageForm:React.FC = () => {
       >
         <Input />
       </Form.Item>
+      </div>
 
       <Form.Item
-        name="password"
-        label="Password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-        hasFeedback
+        name="Message"
+        label="Message"
+        rules={[{ required: true, message: 'Please input Message' }]}
       >
-        <Input.Password />
+        <Input.TextArea showCount maxLength={1000} />
       </Form.Item>
 
-      <Form.Item
-        name="confirm"
-        label="Confirm Password"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          {
-            required: true,
-            message: 'Please confirm your password!',
-          },
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('The new password that you entered do not match!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
+        {/* add robot capture thin here */}
 
-      <Form.Item
-        name="nickname"
-        label="Nickname"
-        tooltip="What do you want others to call you?"
-        rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        name="website"
-        label="Website"
-        rules={[{ required: true, message: 'Please input website!' }]}
-      >
-        <AutoComplete options={websiteOptions} onChange={onWebsiteChange} placeholder="website">
-          <Input />
-        </AutoComplete>
-      </Form.Item>
-
-      <Form.Item
-        name="intro"
-        label="Intro"
-        rules={[{ required: true, message: 'Please input Intro' }]}
-      >
-        <Input.TextArea showCount maxLength={100} />
-      </Form.Item>
-
-      <Form.Item
-        name="gender"
-        label="Gender"
-        rules={[{ required: true, message: 'Please select gender!' }]}
-      >
-        <Select placeholder="select your gender">
-          <Option value="male">Male</Option>
-          <Option value="female">Female</Option>
-          <Option value="other">Other</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item label="Captcha" extra="We must make sure that your are a human.">
-        <Row gutter={8}>
-          <Col span={12}>
-            <Form.Item
-              name="captcha"
-              noStyle
-              rules={[{ required: true, message: 'Please input the captcha you got!' }]}
-            >
-              <Input />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Button>Get captcha</Button>
-          </Col>
-        </Row>
-      </Form.Item>
-
-      <Form.Item
-        name="agreement"
-        valuePropName="checked"
-        rules={[
-          {
-            validator: (_, value) =>
-              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-          },
-        ]}
-        {...tailFormItemLayout}
-      >
-        <Checkbox>
-          I have read the <a href="">agreement</a>
-        </Checkbox>
-      </Form.Item>
-      <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
+      <Form.Item className=''>
+        <Button type="primary" className='bg-[var(--primaryColor)] w-full sm:w-full lg:w-fit shadow-md' htmlType="submit">
+          Submit Feedback
         </Button>
       </Form.Item>
     </Form>
