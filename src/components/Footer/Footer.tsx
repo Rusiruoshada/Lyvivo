86c4f867bda6df4146deeea34f05ec43dd3318d3
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-04 21:22:29 +0530

    style change color bg

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ import BottomFooter from './BottomFooter.tsx';
 const Footer: React.FC = () => {
 
   return (
-    <div className='py-10 sm:py-10 md:!py-10 lg:!py-10  sm:px-1 md:px-10 lg:px-10 bg-[--footerBgColor] text-white flex flex-col '>
+    <div className='py-10 sm:py-10 md:!py-10 lg:!py-10  sm:px-1 md:px-10 lg:px-10 bg-[var(--footerBgColor)] text-white flex flex-col '>
         <UpperFooter />
         <MiddleFooter />
         <BottomFooter />
