f862d61f01adfc3fe2245197ca051b90b0c71bf0
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-07 12:29:03 +0530

    modify app file

--- src/App.js
+++ src/App.js
@@ -2,55 +2,59 @@ import { Provider } from 'react-redux';
 import './App.css';
 import ClientApp from './components/ClientApp.tsx';
 import store from './store/index.ts';
-import {  RouterProvider,  createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import SingleProductPage from './pages/product/[id].tsx';
 import AllProduct from './pages/collection/all.tsx';
 import ErrorPage from './pages/error/Error.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Layout from './components/Layout.tsx';
-import AboutUsPage from './pages/aboutUs/AboutUs.tsx'
+import AboutUsPage from './pages/aboutUs/AboutUs.tsx';
+import { ConfigProvider } from 'antd';
+import antdTheme from './styles/antdconfigTheme.ts';
 
 function App() {
   const queryClient = new QueryClient();
 
   const routers = createBrowserRouter([
     {
-      path:'/',
+      path: '/',
       element: <Layout />,
       errorElement: <ErrorPage />,
       children: [
         {
-          path:'',
-          index:true ,
+          path: '',
+          index: true,
           element: <ClientApp />,
         },
         {
-          path:'/about-us',
-          element: <AboutUsPage /> ,
+          path: '/about-us',
+          element: <AboutUsPage />,
         },
         {
-          path:'/collection/all',
+          path: '/collection/all',
           element: <AllProduct />,
         },
         {
-          path:'/product/:id',
+          path: '/product/:id',
           element: <SingleProductPage />,
         },
         {
           path: '*',
           element: <ErrorPage />,
-        }
-      ]
+        },
+      ],
     },
-  ])
+  ]);
 
   return (
     <>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <RouterProvider router={routers}  />
+          <ConfigProvider theme={{token: antdTheme}}>
+            <RouterProvider router={routers} />
+          </ConfigProvider>
         </Provider>
-    </QueryClientProvider>
+      </QueryClientProvider>
     </>
   );
 }
