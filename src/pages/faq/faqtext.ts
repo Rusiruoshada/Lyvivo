927d083471b2669f098a341d81c3118a9d712a0c
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-08 23:15:24 +0530

    create faq text file for collapse

---
+++ src/pages/faq/faqtext.ts
@@ -0,0 +1,19 @@
+import { CollapseProps } from "antd";
+
+const items: CollapseProps['items'] = [
+    {
+        key: '1',
+        label: 'This is panel header 1',
+        children: {text},
+      },
+      {
+        key: '2',
+        label: 'This is panel header 2',
+        children: {text},
+      },
+      {
+        key: '3',
+        label: 'This is panel header 3',
+        children: {text},
+      },
+]
