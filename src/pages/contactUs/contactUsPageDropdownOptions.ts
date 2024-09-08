9feb97683defd949182e02a85422715a854bfa57
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-08 23:15:57 +0530

    modify contact page dropdown options

---
+++ src/pages/contactUs/contactUsPageDropdownOptions.ts
@@ -0,0 +1,71 @@
+export const dropdownOptionsContactPage = [
+  { label: 'Comments', value: 'Comments' },
+  { label: 'Complaints', value: 'Complaints' },
+];
+
+export const selectOptionsContactPage = [
+  [
+    {
+      value: 'Delivery Related',
+      label: 'Delivery Related',
+    },
+    {
+      value: 'General Enquiry',
+      label: 'General Enquiry',
+    },
+    {
+      value: 'Hamper Related',
+      label: 'Hamper Related',
+    },
+    {
+      value: 'Order Related',
+      label: 'Order Related',
+    },
+    {
+      value: 'Technical Problem',
+      label: 'Technical Problem',
+    },
+    {
+      value: 'Outlet Related',
+      label: 'Outlet Related',
+    },
+    {
+      value: 'Payment Related',
+      label: 'Payment Related',
+    },
+    {
+      value: 'Other',
+      label: 'Other',
+    },
+  ],
+  [
+    {
+      value: 'Promotions',
+      label: 'Promotions',
+    },
+    {
+      value: 'Shopping experience related',
+      label: 'Shopping experience related',
+    },
+    {
+      value: 'Customer service',
+      label: 'Customer service',
+    },
+    {
+      value: 'Inaccurate Pricing',
+      label: 'Inaccurate Pricing',
+    },
+    {
+      value: 'Payments and billing',
+      label: 'Payments and billing',
+    },
+    {
+      value: 'Product availability',
+      label: 'Product availability',
+    },
+    {
+      value: 'Product quality',
+      label: 'Product quality',
+    },
+  ],
+];
