8262a22d7d14dcea71ed2451afd60b94201d351e
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-09 22:31:08 +0530

    add collapse text

--- src/pages/faq/faqtext.ts
+++ src/pages/faq/faqtext.ts
@@ -1,19 +1,100 @@
-import { CollapseProps } from "antd";
 
-const items: CollapseProps['items'] = [
-    {
-        key: '1',
-        label: 'This is panel header 1',
-        children: {text},
-      },
-      {
-        key: '2',
-        label: 'This is panel header 2',
-        children: {text},
-      },
-      {
-        key: '3',
-        label: 'This is panel header 3',
-        children: {text},
-      },
-]
+const collapseText = [
+  {
+    onlineOrderAndDeliveryRelated: {
+      label: 'Online order & delivery related',
+      children: [
+        {
+          question:
+            'Can I place an order for person residing elsewhere within the delivery grid?',
+          answer:
+            'Yes, you can place an order for a recipient other than yourself by stating the delivery address and details accurately prior to checkout.',
+        },
+        {
+          question: 'How do I know my order is confirmed?',
+          answer:
+            'You will receive an order confirmation via email once your order has been placed. If you do not receive an email please contact us on (+94)112303500 and email us on Lyvivocustomercare.jms@Lyvivo.com for further information',
+        },
+        {
+          question: 'Is there a delivery charge for online orders?',
+          answer:
+            'A revised charge of Rs. 325 applies as a transaction processing and delivery fee for all orders placed under the home delivery option. There will be no additional charges for using the Click & Collect service.',
+        },
+        {
+          question:
+            'Is there a restriction on how many orders I can place for day?',
+          answer:
+            'We do not limit the number of orders per day per person at the moment. However, if external environmental factors require a change this rule will be amended without prior notice. Lyvivo reserve the right to not fulfill any orders if deemed to be misusing the site in any way.',
+        },
+        {
+          question:
+            'What happens if you make the delivery to the incorrect address?',
+          answer:
+            'The telephone number of the receiving party is a compulsory field in the order form. We will call and confirm the address and the route before delivering the order and any inaccuracies in the address can be mentioned assuming the address is within the delivery grid of the store the order was placed to.',
+        },
+        {
+          question: 'What happens if I have picked a wrong store by mistake?',
+          answer:
+            'Home delivery orders will get canceled if the delivery suburb and city in the address does not match the delivery grid of the store. The store chosen for Click & Pick cannot be changed at the moment and the order will have to be collected at the stated store.',
+        },
+        {
+          question:
+            'How long does it take for the delivery once I place the order?',
+          answer:
+            'If you place the order before 11a.m (Sri Lankan time) we will deliver the order to you within the same day and orders placed after 11a.m (Sri Lankan time) will be delivered before 8p.m(Sri Lankan time) the next day. Our team will contact the receiver of the order prior to delivery to ensure the time is convenient for him/her. You will receive an email and/or SMS when the order is ready to be delivered. The above is applicable only when there is no disruption to standard operating hours due to any external environmental changes. This can change without prior notice if curfew is imposed.',
+        },
+        {
+          question:
+            'How will I know if my delivery location is covered by Lyvivo?',
+          answer:
+            'You can simply log onto https://www.lyvivo.com/delivery-information to find out in details.',
+        },
+        {
+          question: 'Can I track my order?',
+          answer:
+            'Simply log on to https://lyvivo.com/TrackOrder track the status of your order.Once you receive the order you can confirm the delivery status too.',
+        },
+        {
+          question:
+            'Can I add additional products or change my order once placed?',
+          answer: 'This facility is not available at the moment.',
+        },
+        {
+          question: 'Can I refund items which I received through online order?',
+          answer:
+            'We do not facilitate refunds or replacements for online orders, unless it is a product quality issue, or the Customer feels the standard of the product is below accepted means. Online users are encouraged to place orders with products they require to avoid a situation of refunds or returns. Exchange of products can be considered under exceptional circumstances at the discretion of Lyvivo for a maximum of 3 products only. We will not cash back for returns under any circumstances.',
+        },
+      ],
+    },
+    productRangeAndOtherLimitations: {
+      label: 'Product range & other limitations',
+      children: [
+        {
+          question:
+            'What if the products were available when I ordered, but not when it was going to be delivered?',
+          answer:
+            'Such situations can occur periodically due to stock out situations. You can track your order on https://Lyvivo.com/TrackOrder and you will be able to see the products that have been billed. At the point of placing the order you will also be requested if you prefer a cash refund at the point of delivery or collection or a refund back to the card in a minimum of 5 working days.',
+        },
+        {
+          question: 'Can I purchase any amount of fresh produce?',
+          answer:
+            'For the time being fresh produce can be ordered in multiples of the weight or numbers and for certain products this is fixed.',
+        },
+        {
+          question: 'Is there a limit to the number of products I can buy online?',
+          answer:
+            'This can change depending on the situation where certain restrictions can be placed on some product varieties or promotions. We do not allow bulk purchases via our online platform. Nexus Deals apply to only a maximum of 3 numbers of 3Kg (as applicable) only of the promotional product and for Lyvivo Deals it is 5 numbers or 5Kg (as applicable).',
+        },
+        {
+          question:
+            'Can I find everything in a store through online?',
+          answer:
+            'At least 95% of the products at our stores are reflected in our online range at the moment but availability and range of products vary from store to store.',
+        },
+        
+      ],
+    },
+  },
+];
+
+export default collapseText;
