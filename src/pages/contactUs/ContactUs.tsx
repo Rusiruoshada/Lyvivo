682092de9f51cf8f185a25676ce13eb796f675b3
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-15 19:56:54 +0530

    modify contact page

--- src/pages/contactUs/ContactUs.tsx
+++ src/pages/contactUs/ContactUs.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
-import { Breadcrumb } from 'antd';
+import React, { useState } from 'react';
+import { Breadcrumb, Radio, RadioChangeEvent } from 'antd';
 import { FaFax } from 'react-icons/fa';
 import { MdMail } from 'react-icons/md';
 import { RiPhoneFill } from 'react-icons/ri';
 import ContactPageForm from './ContactPageForm.tsx';
+import {dropdownOptionsContactPage} from './contactUsPageDropdownOptions.ts'
 
-const ContactUs:React.FC = () => {
+const ContactUs: React.FC = () => {
+  
+  const [dropdownSelect, setDropdownSelect] = useState('Comments');
+  
+  const onChangeSelectProblem = ({ target: { value } }: RadioChangeEvent) => {
+    setDropdownSelect(value);
+  };
+
   return (
     <div>
       <div className='flex relative'>
@@ -30,7 +38,7 @@ const ContactUs:React.FC = () => {
           assistance, our Customer Care team is here to assist you (Refer below
           for other ways to contact us)
         </p>
-        <div className='md:grid lg:grid flex flex-col grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-7  text-white gird-rows-4 sm:grid-rows-4 md:grid-rows-2 lg:grid-rows-1 gap-1 [&_p]:m-0 '>
+        <div className='md:grid lg:grid flex flex-col grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-7  text-white gird-rows-4 sm:grid-rows-4 md:grid-rows-2 lg:grid-rows-1 gap-1 [&_p]:m-0 mt-8'>
           <div className='bg-green-600 md:col-span-1 lg:col-span-3 flex lg:flex-row flex-col gap-4 p-3 lg:justify-evenly rounded-tr-[50px] md:!rounded-tl-[50px] md:!rounded-tr-none lg:!rounded-bl-[50px] lg:!rounded-tr-none lg:!rounded-tl-none shadow-md'>
             <h1 className='font-bold text-[48px] items-center justify-center flex m-0'>
               Lyvivo
@@ -62,8 +70,15 @@ const ContactUs:React.FC = () => {
             <p className='text-wrap text-[12px]'>+94 11 230 3500</p>
           </div>
         </div>
-        <div className='my-3'>
-            <ContactPageForm />
+        <div className='my-5'>
+          <Radio.Group
+            options={dropdownOptionsContactPage}
+            onChange={onChangeSelectProblem}
+            value={dropdownSelect}
+            optionType='button'
+            buttonStyle='solid'
+          />
+          <ContactPageForm dropdownSelect={dropdownSelect} />
         </div>
       </div>
     </div>
