f7b12421c3f5c7c75224fd934ce84f9483b6d8c0
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-23 15:17:05 +0530

    modify style and add image to login modal

--- src/components/SignIn/Login.tsx
+++ src/components/SignIn/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import { Modal } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Flex } from 'antd';
+import { Button, Form, Input, Flex, Image, Modal } from 'antd';
 
 interface LoginProps {
   isModalOpen: boolean;
@@ -9,7 +8,6 @@ interface LoginProps {
 }
 
 const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
-
   const [form] = Form.useForm();
   const [usernameFocused, setUsernameFocused] = useState(false);
   const [passwordFocused, setPasswordFocused] = useState(false);
@@ -18,22 +16,20 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
     console.log('Received values of form: ', values);
   };
 
-  console.log(form.getFieldValue('username'))
-
   return (
     <Modal
       width={1000}
       open={isModalOpen}
-      // onCancel={onCancel}
-      className='p-0'
+      onCancel={onCancel}
+      className='[&>div]:!p-0'
+      // rootClassName='!p-0'
       closable={false}
       okButtonProps={{ hidden: true }}
       cancelButtonProps={{ hidden: true }}
       maskClosable
     >
-      <div className='flex flex-row '>
-        <div className='flex flex-col basis-1/2 p-4'>
-          
+      <div className='flex flex-row gap-3'>
+        <div className='flex flex-col basis-1/2 py-10 pl-10 pr-0'>
           <div className='flex flex-col mb-8'>
             <h1 className='text-4xl font-bold text-[var(--primaryColor)]'>
               Login
@@ -43,37 +39,30 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
 
           <Form
             name='login'
-            initialValues={{ remember: true }}
+            initialValues={{ username: '', password: '' }}
             onFinish={onFinish}
             className='w-full'
           >
-            {/* <Form.Item
-              name='username'
-              rules={[
-                { required: true, message: 'Please input your Username!', type: 'email', whitespace: true },
-              ]}
-              validateDebounce={1000}
-              hasFeedback
-            >
-              <Input
-                suffix={<UserOutlined />}
-                placeholder='Username'
-                type='email'
-                size='large'
-                className='!border-0 !border-b-2'
-              />
-            </Form.Item> */}
-             <div className='relative mb-6'>
+            <div className='relative mb-6'>
               <label
                 className={`absolute left-3 top-0 -z-30 transition-all duration-200 transform ${
-                  usernameFocused || form.getFieldValue('username') ? 'visible -translate-y-6 -translate-x-2 scale-100 text-black)] !z-10' : 'translate-y-0 scale-100 -z-30'
+                  usernameFocused
+                    ? 'visible -translate-y-6 -translate-x-2 scale-100 text-gray-500 !z-10'
+                    : 'translate-y-0 scale-100 -z-30'
                 }`}
               >
                 UserName
               </label>
               <Form.Item
                 name='username'
-                rules={[{ required: true, message: 'Please input your Username!', type: 'email', whitespace: true }]}
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please Enter your Username!',
+                    type: 'email',
+                    whitespace: true,
+                  },
+                ]}
                 validateDebounce={1000}
                 hasFeedback
                 className='!mb-0'
@@ -81,40 +70,70 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
                 <Input
                   name='username'
                   id='username'
-                  suffix={<UserOutlined />}
-                  placeholder={`${usernameFocused || form.getFieldValue('username') ?'':'UserName' }`}
+                  prefix={<UserOutlined />}
+                  placeholder={`${usernameFocused ? '' : 'UserName'}`}
                   type='email'
                   size='large'
-                  className='!border-0 !border-b-2'
+                  className='!border-0 !border-b-2 gap-1'
                   onFocus={() => setUsernameFocused(true)}
                   onBlur={() => setUsernameFocused(false)}
                 />
               </Form.Item>
             </div>
-            <Form.Item
-              name='password'
-              rules={[
-                { required: true, message: 'Please input your Password!' },
-                { whitespace:true, message:'Your Password have only spaces, try using characters!'}
-              ]}
-              validateDebounce={1000}
-              hasFeedback
-              className=''
-            >
-              <Input
-                suffix={<LockOutlined />}
-                type='password'
-                placeholder='Password'
-                size='large'
-                className='!border-0 !border-b-2'
+            <div className='relative mt-8'>
+              <label
+                className={`absolute left-3 top-0 -z-30 transition-all duration-200 transform ${
+                  passwordFocused || form.getFieldValue('password')
+                    ? 'visible -translate-y-6 -translate-x-2 scale-100 text-gray-500 !z-10'
+                    : 'translate-y-0 scale-100 -z-30'
+                }`}
+              >
+                Password
+              </label>
+              <Form.Item
                 name='password'
+                rules={[
+                  { required: true, message: 'Please Enter your Password!' },
+                  {
+                    whitespace: true,
+                    message:
+                      'Your Password has only spaces, try using characters!',
+                  },
+                  {
+                    pattern:
+                      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                    message:
+                      'Password must be at least 8 characters long, include uppercase and lowercase letters, a number, and a special character!',
+                  },
+                ]}
+                validateDebounce={1000}
+                hasFeedback
+                className='!mb-0'
                 id='password'
-              />
-              
-            </Form.Item>
+              >
+                <Input.Password
+                  name='password'
+                  id='password'
+                  prefix={<LockOutlined />}
+                  type='password'
+                  placeholder={`${
+                    passwordFocused || form.getFieldValue('password')
+                      ? ''
+                      : 'Password'
+                  }`}
+                  size='large'
+                  className='!border-0 !border-b-2 gap-1'
+                  onFocus={() => setPasswordFocused(true)}
+                  onBlur={() => setPasswordFocused(false)}
+                />
+              </Form.Item>
+            </div>
+
             <Form.Item>
-              <Flex justify='space-between' align='center'>
-                <a href=''>Forgot password</a>
+              <Flex justify='end' align='center'>
+                <Button type='link' className='text-gray-400 p-0'>
+                  Forgot password?
+                </Button>
               </Flex>
             </Form.Item>
 
@@ -125,15 +144,42 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
                 className={`!bg-[var(--primaryColor)] disabled:!bg-gray-100`}
                 htmlType='submit'
                 size='large'
-                
               >
                 Log in
               </Button>
-              or <a href=''>Register now!</a>
             </Form.Item>
           </Form>
+          <div className='flex flex-row gap-2'>
+            <p className='!text-[15px]'>Don't have an account? </p>
+            <Button
+              type='link'
+              className='p-0 h-fit font-medium text-gray-600 text-[14px]'
+            >
+              Sign Up
+            </Button>
+          </div>
         </div>
-        <div className='basis-1/2'></div>
+        <div className='basis-1/2 p-1 shadow-lg'>
+          <div className='h-full text-white relative'>
+            <div className='z-50 absolute top-6 text-center'>
+              <h2 className='text-5xl mb-2 font-black '>Welcome back!</h2>
+              <p className='text-2xl font-semibold mx-auto text-wrap w-[95%]'>
+                Welcome to Lyvivo, where exceptional customer care meets top
+                product quality!
+              </p>
+            </div>
+
+            <div className='h-full relative'>
+              <div className='absolute top-0 bottom-0 left-0 right-0 h-full !z-[1] opacity-40 rounded-md bg-black'/>
+              <div
+                style={{
+                  backgroundImage: "url('/images/login/login image1.jpeg')",
+                }}
+                className='absolute top-0 bottom-0 right-0 left-0 h-full w-full bg-cover bg-center z-0 opacity-100 rounded-md'
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </Modal>
   );
