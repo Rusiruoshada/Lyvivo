db86bb616b03c19f705e8f4f85a391a175fa9518
Rusiru_Oshada <rusirusamaraweera749@gmail.com>
2024-09-24 22:19:34 +0530

    modify design for more responsive

--- src/components/SignIn/Login.tsx
+++ src/components/SignIn/Login.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Flex, Image, Modal } from 'antd';
+import { Button, Form, Input, Flex, Modal } from 'antd';
 
 interface LoginProps {
   isModalOpen: boolean;
@@ -13,22 +13,23 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
   const [passwordFocused, setPasswordFocused] = useState(false);
 
   const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
+    console.log('Received values of form: ', values);    
+    form.resetFields();
   };
 
   return (
     <Modal
-      width={1000}
       open={isModalOpen}
       onCancel={onCancel}
-      className='[&>div]:!p-0'
-      // rootClassName='!p-0'
+      onOk={onCancel}
+      className='[&>div]:!p-0 w-full sm:!w-full md:!w-full lg:!w-[1200px]'
       closable={false}
+      footer={''}
       okButtonProps={{ hidden: true }}
       cancelButtonProps={{ hidden: true }}
       maskClosable
     >
-      <div className='flex flex-row gap-3'>
+      <div className='flex flex-row gap-5'>
         <div className='flex flex-col basis-1/2 py-10 pl-10 pr-0'>
           <div className='flex flex-col mb-8'>
             <h1 className='text-4xl font-bold text-[var(--primaryColor)]'>
@@ -39,14 +40,15 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
 
           <Form
             name='login'
+            form={form}
             initialValues={{ username: '', password: '' }}
             onFinish={onFinish}
             className='w-full'
           >
-            <div className='relative mb-6'>
+            <div className='relative mb-8'>
               <label
                 className={`absolute left-3 top-0 -z-30 transition-all duration-200 transform ${
-                  usernameFocused
+                  usernameFocused || form.isFieldTouched('username')
                     ? 'visible -translate-y-6 -translate-x-2 scale-100 text-gray-500 !z-10'
                     : 'translate-y-0 scale-100 -z-30'
                 }`}
@@ -71,7 +73,7 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
                   name='username'
                   id='username'
                   prefix={<UserOutlined />}
-                  placeholder={`${usernameFocused ? '' : 'UserName'}`}
+                  placeholder={`${usernameFocused || form.getFieldValue('username') ? '' : 'UserName'}`}
                   type='email'
                   size='large'
                   className='!border-0 !border-b-2 gap-1'
@@ -80,7 +82,7 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
                 />
               </Form.Item>
             </div>
-            <div className='relative mt-8'>
+            <div className='relative'>
               <label
                 className={`absolute left-3 top-0 -z-30 transition-all duration-200 transform ${
                   passwordFocused || form.getFieldValue('password')
@@ -141,7 +143,7 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
               <Button
                 block
                 type='primary'
-                className={`!bg-[var(--primaryColor)] disabled:!bg-gray-100`}
+                className={`!bg-[var(--primaryColor)] disabled:!bg-gray-100 text-white font-bold`}
                 htmlType='submit'
                 size='large'
               >
@@ -169,13 +171,13 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel }) => {
               </p>
             </div>
 
-            <div className='h-full relative'>
-              <div className='absolute top-0 bottom-0 left-0 right-0 h-full !z-[1] opacity-40 rounded-md bg-black'/>
+            <div className='h-[500px] relative'>
+              <div className='absolute top-0 bottom-0 left-0 right-0 h-full !z-[1] opacity-50 rounded-md bg-black' />
               <div
                 style={{
                   backgroundImage: "url('/images/login/login image1.jpeg')",
                 }}
-                className='absolute top-0 bottom-0 right-0 left-0 h-full w-full bg-cover bg-center z-0 opacity-100 rounded-md'
+                className='absolute top-0 bottom-0 right-auto left-2 h-full w-full bg-cover bg-center z-0 opacity-100 rounded-md scale-105 shadow-lg'
               />
             </div>
           </div>
