637f4db19c39a9303e3f406061156e92e8f889e8
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-10-28 23:45:05 +0530

    modify login

--- src/components/SignIn/Login.tsx
+++ src/components/SignIn/Login.tsx
@@ -21,6 +21,7 @@ const Login: React.FC<LoginProps> = ({ isModalOpen, onCancel,onClickOnSignUpOrLo
       await axios.post('https://localhost/api/login', {
         ...values,
       })
+      
       form.resetFields();
     } catch(error) {
       console.log('error with login frontend', error)
