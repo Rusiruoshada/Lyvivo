5c825bd0145c766c5dba93bd205dc7d13e5d6649
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-11-18 23:13:03 +0530

    create checkoutForm

--- src/components/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import { useStripe, useElements } from '@stripe/react-stripe-js';
 import { PaymentElement } from '@stripe/react-stripe-js';
-import { Form } from 'antd';
+import { Form, Modal } from 'antd';
 
-const CheckoutForm: React.FC = () => {
+interface CheckoutFormProps{
+    isOpen: boolean;
+
+}
+
+
+const CheckoutForm: React.FC<CheckoutFormProps> = (isOpen) => {
     
     const stripe = useStripe();
     const elements = useElements();
 
     return (
-        
+    <Modal
+      open={isOpen}
+      onCancel={() => onCancel({ register: false })}
+      className="[&>div]:!p-0 w-full sm:!w-full md:!w-full lg:!w-[1200px]"
+      closable={false}
+      footer={""}
+      okButtonProps={{ hidden: true }}
+      cancelButtonProps={{ hidden: true }}
+      maskClosable
+    >
         <Form>
             <PaymentElement/>
            
         </Form>
+    </Modal>
     )
 }
 
