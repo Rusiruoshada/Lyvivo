b2743aa475f66b781ec470aefc5fb1f9824894da
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-12-06 00:33:24 +0530

    add handleSubmit to checkoutForm

--- src/components/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
-import { useStripe, useElements } from '@stripe/react-stripe-js';
-import { PaymentElement } from '@stripe/react-stripe-js';
-import { Button, Form, Modal } from 'antd';
+import React, { useState } from "react";
+import { useStripe, useElements } from "@stripe/react-stripe-js";
+import { PaymentElement } from "@stripe/react-stripe-js";
+import { Button, Form, Modal } from "antd";
 
 interface CheckoutFormProps {
   isModalOpen: boolean;
   onCancel: any;
 }
 
-
-const CheckoutForm: React.FC<CheckoutFormProps> = ({isModalOpen, onCancel}) => {
+const CheckoutForm: React.FC<CheckoutFormProps> = ({
+  isModalOpen,
+  onCancel,
+}) => {
   const stripe = useStripe();
   const elements = useElements();
+  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+  const [isMessage, setIsMessage] = useState<any>('')
 
-  const handleSubmit = async (event:any) => {
-    event.preventDefault(); 
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    if (!stripe || !elements) return;
 
-  }
+    setIsProcessing(true);
 
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: `${window.location.origin}/`
+      }
+    })
+
+    if (error) {
+      setIsMessage(error);
+    }
+
+    setIsProcessing(false)
+  };
 
   return (
     <Modal
@@ -33,10 +51,16 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({isModalOpen, onCancel}) => {
     >
       <Form id="payment-form" onSubmitCapture={handleSubmit}>
         <PaymentElement />
-        <Button  type='primary' className='bg-[var(--primaryColor)] mt-3 w-full' >Pay Now</Button>
+        <Button
+          disabled={isProcessing}
+          type="primary"
+          className="bg-[var(--primaryColor)] mt-3 w-full"
+        >
+          {isProcessing ? "Processing ..." : "Pay Now"}
+        </Button>
       </Form>
     </Modal>
   );
 };
 
-export default CheckoutForm
+export default CheckoutForm;
