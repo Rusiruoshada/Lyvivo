dde6dbf610e8a4b5e2f2dc24cca51e0a9d15c64c
Rusiruoshada <rusirusamaraweera749@gmail.com>
2024-12-05 23:43:24 +0530

    modify checkoutForm

--- src/components/CheckoutForm/CheckoutForm.tsx
+++ src/components/CheckoutForm/CheckoutForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useStripe, useElements } from '@stripe/react-stripe-js';
 import { PaymentElement } from '@stripe/react-stripe-js';
-import { Form, Modal } from 'antd';
+import { Button, Form, Modal } from 'antd';
 
 interface CheckoutFormProps {
   isModalOpen: boolean;
@@ -13,21 +13,27 @@ const CheckoutForm: React.FC<CheckoutFormProps> = ({isModalOpen, onCancel}) => {
   const stripe = useStripe();
   const elements = useElements();
 
+  const handleSubmit = async (event:any) => {
+    event.preventDefault(); 
+
+  }
+
+
   return (
     <Modal
       open={isModalOpen}
-      onCancel={() => onCancel}
+      onCancel={() => onCancel()}
       className="[&>div]:!p-0 w-full sm:!w-full md:!w-full lg:!w-[700px] p
       -10"
-      rootClassName='bg-black'
       closable={false}
       footer={""}
       okButtonProps={{ hidden: true }}
       cancelButtonProps={{ hidden: true }}
       maskClosable
     >
-      <Form>
+      <Form id="payment-form" onSubmitCapture={handleSubmit}>
         <PaymentElement />
+        <Button  type='primary' className='bg-[var(--primaryColor)] mt-3 w-full' >Pay Now</Button>
       </Form>
     </Modal>
   );
