465d4b62b7dc3b59a771b84e43ec7b2d4b85738b
Rusiruoshada <rusirusamaraweera749@gmail.com>
2025-02-07 17:45:29 +0530

    modify searchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -3,36 +3,76 @@ import { Button, Input, Space } from 'antd';
 import type { SearchProps } from 'antd/es/input/Search';
 import { MdSearch } from 'react-icons/md';
 
+interface SearchBarProps {
+  position?: "relative" | "absolute" | "fixed" | "sticky" | "static";
+  borderRadius?: string;
+  bottom?: string;
+  right?: string;
+  left?: string;
+  marginRight?: string;
+  marginLeft?: string;
+  color?: string;
+  backgroundColor?: string;
+  className?: string;
+  buttonClassName?:string;
+}
+
 const { Search } = Input;
 
 const onSearch: SearchProps['onSearch'] = (value, _e, info) =>{
   console.log(info?.source, value);
 }
 
-const customButton = <Button type="primary" icon={<MdSearch className='text-2xl' />} />;
 
-const SearchBar: React.FC = () => (
-  <Space direction='vertical' >
-    <Search
-      placeholder='input search text'
-      enterButton={customButton}
-      allowClear
-      size='large'
-      onSearch={onSearch}
+const SearchBar: React.FC<SearchBarProps> = ({
+  className,
+  position,
+  borderRadius,
+  bottom,
+  right,
+  left,
+  marginLeft,
+  marginRight,
+  color,
+  backgroundColor,
+  buttonClassName,
+}) => {
+  const customButton = (
+    <Button
+      type="primary"
       style={{
-        position: 'absolute',
-        bottom: 'auto',
-        right: '0px',
-        left: '0px',
-        marginRight: 'auto',
-        marginLeft: 'auto',
-        backgroundColor: 'var(--primaryColor)',
-        borderRadius: '8px',
-        color: '#ffffff',
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        
       }}
-      className='w-[95%] sm:w-[95%] md:w-[80%] lg:w-[65%] top-[70px] sm:top-[70px] md:top-[65px] lg:top-[65px]'
+      icon={<MdSearch className="text-2xl" />}
     />
-  </Space>
-);
+  );
+
+  return (
+    <Space direction="vertical">
+      <Search
+        placeholder="input search text"
+        enterButton={customButton}
+        allowClear
+        size="middle"
+        onSearch={onSearch}
+        style={{
+          position: position,
+          bottom: bottom,
+          right: right,
+          left: left,
+          marginRight: marginRight,
+          marginLeft: marginLeft,
+          color: color,
+          backgroundColor: backgroundColor,
+          borderRadius: borderRadius,
+        }}
+        className={className}
+      />
+    </Space>
+  );
+};
 
 export default SearchBar;
