12c8c824f99feb2587a95f837ae6f27475d7fcb2
Rusiruoshada <rusirusamaraweera749@gmail.com>
2025-03-03 15:16:54 +0530

    add new routes and elements for admin

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { Provider } from 'react-redux';
-import './App.css';
-import ClientApp from './components/ClientApp.tsx';
-import store from './store/index.ts';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import SingleProductPage from './pages/product/[id].tsx';
-import AllProduct from './pages/collection/all.tsx';
-import ErrorPage from './pages/error/Error.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Layout from './components/Layout.tsx';
-import AboutUsPage from './pages/aboutUs/AboutUs.tsx';
-import ContactUs from './pages/contactUs/ContactUs.tsx'
-import { ConfigProvider } from 'antd';
-import antdTheme from './styles/antdconfigTheme.ts';
-import FAQ from './pages/faq/FAQ.tsx';
-import React from 'react';
-import AdminPage from './pages/admin/AdminPage.tsx';
+import { Provider } from "react-redux";
+import "./App.css";
+import ClientApp from "./components/ClientApp.tsx";
+import store from "./store/index.ts";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SingleProductPage from "./pages/product/[id].tsx";
+import AllProduct from "./pages/collection/all.tsx";
+import ErrorPage from "./pages/error/Error.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Layout from "./components/Layout.tsx";
+import AboutUsPage from "./pages/aboutUs/AboutUs.tsx";
+import ContactUs from "./pages/contactUs/ContactUs.tsx";
+import { ConfigProvider } from "antd";
+import antdTheme from "./styles/antdconfigTheme.ts";
+import FAQ from "./pages/faq/FAQ.tsx";
+import React from "react";
+import ProductList from "./components/AdminUI/ProductList/ProductList.tsx";
+import DashBoard from "./components/AdminUI/Dashboard/DashBoard.tsx";
+import AdminLogin from "./components/AdminUI/AdminLogin/AdminLogin.tsx";
+import PrivateRouter from "./components/PrivateRouter/PrivateRouter.tsx";
 
 function App() {
+
+
+
   const queryClient = new QueryClient();
 
   const routers = createBrowserRouter([
@@ -24,6 +30,7 @@ function App() {
       path: "/",
       element: <Layout />,
       errorElement: <ErrorPage />,
+      caseSensitive: false,
       children: [
         {
           path: "",
@@ -57,12 +64,30 @@ function App() {
       ],
     },
     {
-      path: "/admin",
-      element: <AdminPage />,
+      path: "/adminLogin",
+      element: <AdminLogin  />,
       errorElement: <ErrorPage />,
+    },
+    {
+      path: "/admin",
+      element: <PrivateRouter />,
+      errorElement: <ErrorPage />,
+      caseSensitive: false,
       children: [
-        
-      ]
+        {
+          path: "/admin",
+          element: <DashBoard />,
+          index: true,
+        },
+        {
+          path: "/admin/productList",
+          element: <ProductList />,
+        },
+        {
+          path: "*",
+          element: <ErrorPage />,
+        },
+      ],
     },
   ]);
 
@@ -70,7 +95,7 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
-          <ConfigProvider theme={{token: antdTheme}}>
+          <ConfigProvider theme={{ token: antdTheme }}>
             <RouterProvider router={routers} />
           </ConfigProvider>
         </Provider>
